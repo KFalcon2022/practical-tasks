e44bf36835e62b0c01e5b8811d98622eed0fbdfc
Yahor <egor.2000.xaxaxa@gmail.com>
2022-11-26 23:57:23 +0300

    Lessons 13-14

---
+++ src/com/walking/lesson13_inheritance/task1/Main.java
@@ -0,0 +1,6 @@
+package com.walking.lesson13_inheritance.task1;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/com/walking/lesson14_polymorphism/task2/Main.java
@@ -0,0 +1,6 @@
+package com.walking.lesson14_polymorphism.task2;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
