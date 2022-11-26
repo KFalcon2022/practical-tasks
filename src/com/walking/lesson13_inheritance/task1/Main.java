39bd4a5de7cc5e7de14b704862f41af05ffa3af7
Yahor <egor.2000.xaxaxa@gmail.com>
2022-11-26 23:57:23 +0300

    Lessons 13-14

--- src/com/walking/lesson13_inheritance/task1/Main.java
+++ src/com/walking/lesson13_inheritance/task1/Main.java
@@ -1,27 +1,6 @@
 package com.walking.lesson13_inheritance.task1;
 
-import com.walking.lesson13_inheritance.task1.model.Cat;
-import com.walking.lesson13_inheritance.task1.model.Dog;
-
-/**
- * Реализовать класс Animal, содержащий protected-конструктор без параметров,
- * который выводит в консоль сообщение «I’m an animal».
- * <p>
- * Реализовать для Animal классы-наследники Dog и Cat.
- * Реализовать для каждого из них приватный конструктор без параметров,
- * который выводит в консоль сообщение «I’m a dog» («I’m a cat»).
- * Также реализовать публичный конструктор, принимающий строковый параметр color.
- * Он должен вызывать конструктор без параметров,
- * а также выводить в консоль сообщение «I’m a <color> dog» («I’m a <color> cat»),
- * где <color> – значение параметра конструктора color.
- * <p>
- * Создайте в main() экземпляр Dog и экземпляр Cat.
- * Обратите внимание на консоль.
- * Такой ли порядок сообщений вы ожидали увидеть?
- */
 public class Main {
     public static void main(String[] args) {
-        new Dog("black");
-        new Cat("white");
     }
 }
--- src/com/walking/lesson14_polymorphism/task2/Main.java
+++ src/com/walking/lesson14_polymorphism/task2/Main.java
@@ -1,28 +1,6 @@
 package com.walking.lesson14_polymorphism.task2;
 
-import com.walking.lesson14_polymorphism.task2.model.Animal;
-import com.walking.lesson14_polymorphism.task2.model.Cat;
-import com.walking.lesson14_polymorphism.task2.model.Cow;
-import com.walking.lesson14_polymorphism.task2.model.Dog;
-
-/**
- * Используя новые знания, упростите реализацию задачи 2 из предыдущего урока:
- * <a href="https://telegra.ph/Nasledovanie-Klyuchevoe-slovo-extends-i-ispolzovanie-protected-Klyuchevoe-slovo-super-Privedenie-ssylochnyh-tipov-11-23">...</a>
- * Допустимо использовать метод sound() вместо woof(), meow() и moo().
- * Метод sound() допустимо сделать публичным.
- * <p>
- * Использовать отдельное поле для хранения выводимой строки — недопустимо:)
- */
 public class Main {
     public static void main(String[] args) {
-        Animal[] animals = new Animal[]{new Cat(), new Dog(), new Cow()};
-
-        soundAll(animals);
-    }
-
-    private static void soundAll(Animal[] animals) {
-        for (Animal animal : animals) {
-            animal.sound();
-        }
     }
 }
