1ab4a61fb2dc5c4bf2bd131ff74f47dbbe2edcc0
TheVod <yaroslaw.krivoshapov@yandex.ru>
2022-11-28 22:16:51 +0300

    Тест коммита

---
+++ src/com/walking/lesson14_polymorphism/task1/shapes/Square.java
@@ -0,0 +1,7 @@
+package com.walking.lesson14_polymorphism.task1.shapes;
+
+public class Square extends Shape {
+    public Square(int length) {
+        super(length);
+    }
+}
---
+++ src/com/walking/lesson14_polymorphism/task1/shapes/Triangle.java
@@ -0,0 +1,7 @@
+package com.walking.lesson14_polymorphism.task1.shapes;
+
+public class Triangle extends Shape{
+    public Triangle(int length) {
+        super(length);
+    }
+}
