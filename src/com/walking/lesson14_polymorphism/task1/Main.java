373278d5fe41a212447f85302812d1b625b030af
TheVod <yaroslaw.krivoshapov@yandex.ru>
2022-11-28 22:14:58 +0300

    Тест коммита

--- src/com/walking/lesson14_polymorphism/task1/Main.java
+++ src/com/walking/lesson14_polymorphism/task1/Main.java
@@ -1,6 +1,29 @@
 package com.walking.lesson14_polymorphism.task1;
 
+import com.walking.lesson14_polymorphism.task1.shapes.Shape;
+import com.walking.lesson14_polymorphism.task1.shapes.Square;
+import com.walking.lesson14_polymorphism.task1.shapes.Triangle;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите тип фигуры:\n1: Треугольник\n2: Квадрат");
+        int type = scanner.nextInt();
+        System.out.println("Введите длину сторон фигуры:");
+        int length = scanner.nextInt();
+        scanner.close();
+
+        Shape shape = createShape(type, length);
+        System.out.println(shape);
+    }
+
+    static Shape createShape (int type, int length) {
+        return switch (type) {
+            case 1 -> new Triangle(length);
+            case 2 -> new Square(length);
+            default -> null;
+        };
     }
 }
