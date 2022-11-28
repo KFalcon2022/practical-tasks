3ad7bd863914bd6599a4357e958e4d7089c4879b
Yahor <egor.2000.xaxaxa@gmail.com>
2022-11-28 23:21:48 +0300

    Lessons 19-21

---
+++ src/com/walking/lesson20_exceptions/task1_throwsException/Main.java
@@ -0,0 +1,6 @@
+package com.walking.lesson20_exceptions.task1_throwsException;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/com/walking/lesson20_exceptions/task2/Main.java
@@ -0,0 +1,6 @@
+package com.walking.lesson20_exceptions.task2;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
