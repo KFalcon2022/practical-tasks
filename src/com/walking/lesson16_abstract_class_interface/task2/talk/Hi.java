a9b748aecb23df21739fd9969935e8e0a5c8fed3
SergioZ14 <sergioz140592@gmail.com>
2022-11-30 13:58:21 +0300

    replacing a class with an interface

---
+++ src/com/walking/lesson16_abstract_class_interface/task2/talk/Hi.java
@@ -0,0 +1,9 @@
+package com.walking.lesson16_abstract_class_interface.task2.talk;
+
+public interface Hi {
+    String s = "Hi";
+
+    static void print() {
+        System.out.println(s);
+    }
+}
---
+++ src/com/walking/lesson16_abstract_class_interface/task2/talk/How.java
@@ -0,0 +1,10 @@
+package com.walking.lesson16_abstract_class_interface.task2.talk;
+
+public interface How {
+    String s = "How are your doing";
+
+    static void print() {
+        System.out.println(s);
+    }
+
+}
