b4d15ce8f63da3c8b1436dd1db0f68fef49644ce
SergioZ14 <sergioz140592@gmail.com>
2022-11-30 13:13:58 +0300

    replacing a class with an interface

--- src/com/walking/lesson16_abstract_class_interface/task1/Main.java
+++ src/com/walking/lesson16_abstract_class_interface/task1/Main.java
@@ -1,6 +1,46 @@
 package com.walking.lesson16_abstract_class_interface.task1;
 
+import com.walking.lesson16_abstract_class_interface.task1.model.EquilateralShape;
+import com.walking.lesson16_abstract_class_interface.task1.model.EquilateralTriangle;
+import com.walking.lesson16_abstract_class_interface.task1.model.Square;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter shape's length: ");
+        int length = scanner.nextInt();
+
+        System.out.print("Enter shape's type:\n 1: Square\n 2: Triangle\n");
+        int shapeType = scanner.nextInt();
+
+        scanner.close();
+
+        if (length < 1 || (shapeType != 1 && shapeType != 2)) {
+            System.out.println("Incorrect input.");
+            return;
+        }
+
+        String shapeString = createShapeString(length, shapeType);
+        System.out.println(shapeString);
+    }
+
+    private static String createShapeString(int length, int type) {
+        EquilateralShape shape;
+
+        switch (type) {
+            case 1:
+                shape = new Square(length);
+                break;
+            case 2:
+                shape = new EquilateralTriangle(length);
+                break;
+            default:
+                return "Unknown shape";
+        }
+
+        return shape.createShapeString();
     }
 }
