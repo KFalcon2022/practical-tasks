197e557d404a963fd88497a129806540f5a73ad6
SergioZ14 <sergioz140592@gmail.com>
2022-11-30 13:58:21 +0300

    replacing a class with an interface

--- src/com/walking/lesson16_abstract_class_interface/task2/Main.java
+++ src/com/walking/lesson16_abstract_class_interface/task2/Main.java
@@ -1,6 +1,20 @@
 package com.walking.lesson16_abstract_class_interface.task2;
 
-public class Main {
-    public static void main(String[] args) {
+import com.walking.lesson16_abstract_class_interface.task2.talk.Bye;
+import com.walking.lesson16_abstract_class_interface.task2.talk.Hi;
+import com.walking.lesson16_abstract_class_interface.task2.talk.How;
+import com.walking.lesson16_abstract_class_interface.task2.talk.Phrases;
+
+import java.util.Scanner;
+
+public class Main implements Hi, Bye, How {
+        public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter string: ");
+            String s = scanner.nextLine();
+            scanner.close();
+
+            Phrases phrase = new Phrases(s);
+        }
     }
-}
