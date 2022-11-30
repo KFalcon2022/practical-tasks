ff268add8f788684cc33d150a8caac093d4d72c8
Yahor <egor.2000.xaxaxa@gmail.com>
2022-11-30 21:53:11 +0300

    Refactoring

--- src/com/walking/lesson3_casts_conditional_constructions/Task3Printf.java
+++ src/com/walking/lesson3_casts_conditional_constructions/Task3Printf.java
@@ -1,12 +1,12 @@
-package com.walking.lesson3;
+package com.walking.lesson3_casts_conditional_constructions;
 
 /**
  * Завести три переменные типа String.
  * С клавиатуры ввести в них вашу фамилию, имя и отчество.
  * Вывести в консоль в одну строку ваше ФИО.
- * Сделать, используя System.out.println
+ * Сделать, используя System.out.printf
  */
-public class Task3Println {
+public class Task3Printf {
     public static void main(String[] args) {
     }
 }
