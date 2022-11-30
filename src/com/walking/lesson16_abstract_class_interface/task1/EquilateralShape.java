0111b099eaee97a9a54fc672894128be910e9fc6
kott707 <234782@mail.ru>
2022-11-30 22:40:54 +0500

    changed all, using interface;

---
+++ src/com/walking/lesson16_abstract_class_interface/task1/EquilateralShape.java
@@ -0,0 +1,11 @@
+package src.com.walking.lesson16_abstract_class_interface.task1;
+
+public interface EquilateralShape {
+    String EMPTY_ELEMENT = " ";
+    String EMPTY_STRING = "";
+    char NEW_LINE_SYMBOL = '\n';
+
+      String createShapeString();
+
+
+}
