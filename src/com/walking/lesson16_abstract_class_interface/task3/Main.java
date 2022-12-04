e8b6ffc647614f37ff3c141d00e4b1ae42142a89
funkyuo <118391571+funkyuo@users.noreply.github.com>
2022-12-04 21:28:23 +0300

    Lesson 16 Task3 - task (Lesson 13 Task 2) refactoring using "Abstract class"

--- src/com/walking/lesson16_abstract_class_interface/task3/Main.java
+++ src/com/walking/lesson16_abstract_class_interface/task3/Main.java
@@ -1,6 +1,39 @@
 package com.walking.lesson16_abstract_class_interface.task3;
 
+import com.walking.lesson16_abstract_class_interface.task3.model.*;
+
+
+/**
+ * Реализуйте класс Animal. Реализуйте его наследников: Dog, Cat, Cow.
+ * Каждый из наследников должен содержать свой метод: woof(), meow() и moo() соответственно.
+ * Остальные поля и методы суперкласса и наследников реализовать по своему усмотрению,
+ * если они необходимы.
+ * <p>
+ * Каждый из методов должен выводить в консоль соответствующую ему строку:
+ * "woof ", "meow " или "moo".
+ * <p>
+ * В main() создать и наполнить в произвольном порядке
+ * объектами разных классов-наследников массив типа Animal.
+ * Реализовать метод, принимающий массив Animal и вызывающий метод,
+ * характерный для конкретного животного.
+ * <p>
+ * Использовать при решении instanceof, getClass() или другие неизвестные нам механики – недопустимо.
+ * <p>
+ * Дополнительное условие (необязательно): решить задачу, при условии,
+ * что woof(), meow() и moo() внутри себя выполняют только вызов protected-метода sound(),
+ * который определен в Animal.
+ */
 public class Main {
     public static void main(String[] args) {
+        Animal[] animals = new Animal[]{new Cat(), new Dog(), new Cow()};
+        soundAll(animals);
+
     }
+
+    private static void soundAll(Animal[] animals) {
+        for (Animal animal : animals) {
+            animal.sound();
+        }
+    }
+
 }
---
+++ src/com/walking/lesson16_abstract_class_interface/task3/model/Animal.java
@@ -0,0 +1,7 @@
+package com.walking.lesson16_abstract_class_interface.task3.model;
+
+public abstract class Animal {
+    public abstract void sound();
+    //public void sound(){/*body*/};
+
+}
