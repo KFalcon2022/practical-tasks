25edb9bed825f91ef633b1149d2ff00c2281ccad
SergioZ14 <sergioz140592@gmail.com>
2022-12-05 09:25:49 +0300

    Убрал переменную name, добавил @Override

--- src/com/walking/lesson16_abstract_class_interface/task3/model/Animal.java
+++ src/com/walking/lesson16_abstract_class_interface/task3/model/Animal.java
@@ -1,11 +1,6 @@
 package com.walking.lesson16_abstract_class_interface.task3.model;
 
 public abstract class Animal {
-    private final String name;
-
-    public Animal(String name) {
-        this.name = name;
-    }
 
     public abstract void sound();
 
