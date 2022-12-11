ba9f96de7eaab6f29d4ff506af8aeba340756801
Zawadskiy Serhii <srgzavadsky@gmail.com>
2022-12-11 13:52:27 +0200

    lesson 25-26

--- src/com/walking/lesson25_nested_classes/task1/static_class/Main.java
+++ src/com/walking/lesson25_nested_classes/task1/static_class/Main.java
@@ -7,5 +7,35 @@ package com.walking.lesson25_nested_classes.task1.static_class;
  */
 public class Main {
     public static void main(String[] args) {
+        Animal[] animals = new Animal[]{new Cat(), new Dog(), new Cow()};
+        soundAll(animals);
+    }
+
+    private static void soundAll(Animal[] animals) {
+        for (Animal animal : animals) {
+            animal.sound();
+        }
+    }
+
+    public static abstract class Animal {
+        public abstract void sound();
+    }
+
+    public static class Dog extends Animal {
+        public void sound() {
+            System.out.println("woof");
+        }
+    }
+
+    public static class Cat extends Animal {
+        public void sound() {
+            System.out.println("meow");
+        }
+    }
+
+    public static class Cow extends Animal {
+        public void sound() {
+            System.out.println("moo");
+        }
     }
 }
