c791638a87d19ccaf9f0a1cb7751e5f15cc864c2
kott707 <234782@mail.ru>
2022-12-14 12:31:11 +0500

    просто тест

--- src/com/walking/lesson26_string_types/task1/Main.java
+++ src/com/walking/lesson26_string_types/task1/Main.java
@@ -4,6 +4,7 @@ package com.walking.lesson26_string_types.task1;
  * Реализуйте задачу
  * <a href="https://github.com/KFalcon2022/practical-tasks/blob/master/src/com/walking/lesson6_methods/Task3.java">...</a>
  * используя StringBuilder или StringBuffer. Объясните свой выбор.
+ * тест ПР
  */
 public class Main {
     public static void main(String[] args) {
