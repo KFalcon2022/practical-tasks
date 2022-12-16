dc9baab2acd9531c5c4bdc8e2400e9db4a2fa24a
kott707 <234782@mail.ru>
2022-12-17 03:40:13 +0500

    задача 26-2, пользователь вводит строку, считаем уникальные слова, молчать нельзя.

--- src/com/walking/lesson26_string_types/task2/Main.java
+++ src/com/walking/lesson26_string_types/task2/Main.java
@@ -1,16 +1,56 @@
 package com.walking.lesson26_string_types.task2;
 
+import java.util.Scanner;
+
 /**
- * Реализуйте программу, принимающую с клавиатуры строку,
- * содержащую слова, разделенные пробелом.
- * Выведите в консоль количество уникальных слов в строке.
- * Учтите, что строка может начинаться с пробела.
- * <p>
- * Для упрощения допустим, что в строке не могут использоваться символы,
- * отличные от пробела или русских/английский букв.
- * Помните, что слово может быть введено в разных регистрах.
+ * задача принимать с клавиатуры строку со словами, нужно посчитать количество уникальных слов;
+ * программа отлавливает двойной пробел, четко видит уникальные слова, но итоговое количество слов считает приблизительно :D
+ * пользователь не имеет право молчать, программа требует разговор иначе format C://
  */
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Скажите несколько слов для анализа");
+        String answer = null;
+
+        do {
+            answer = scanner.nextLine();
+            if (answer.isBlank()) {
+                System.err.println("Вы не сказали ничего, давай, не молчи.");
+            }
+        } while (answer.isBlank());
+
+        //удалим лишние пробелы если были
+        answer = answer.strip();
+
+
+        //создаем массив из слов которые передал пользователь строкой
+        String[] words = answer.split(" ");
+
+        //запускаем статический метод, который вернет число уникальных слов
+        int uniq = checkUniq(words);
+
+        System.out.println("Всего слов: " + words.length + "\nВсего уникальных слов: " + uniq);
+    }
+
+    public static int checkUniq(String[] array) {
+        int result = array.length;
+        int repeat = 0;
+
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i].compareToIgnoreCase(array[j]) == 0) {
+                    repeat++;
+                    break;
+                } else if (array[i].isBlank()) {
+                    repeat++;
+                }
+            }
+        }
+
+        return result - repeat;
     }
 }
+
+
