ae21fa300e0b64e9f18bc4862b628d20241458cb
Yahor <egor.2000.xaxaxa@gmail.com>
2022-12-21 18:58:23 +0300

    Lessons 33-34

---
+++ src/com/walking/lesson33_files_2/task2/Main.java
@@ -0,0 +1,15 @@
+package com.walking.lesson33_files_2.task2;
+
+/**
+ * Реализуйте Задачу 2 из урока
+ * <a href="https://telegra.ph/Rabota-s-fajlami-CHast-I-12-17">...</a>
+ * с помощью:
+ * <p>
+ * Вариант 1: FileReader;
+ * Вариант 2: BufferedInputStream;
+ * Вариант 3: BufferedReader.
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
