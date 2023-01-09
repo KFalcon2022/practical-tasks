85ffa70d33475092749f32387a055c444ea3d5a8
Zawadskiy Serhii <srgzavadsky@gmail.com>
2023-01-09 18:00:48 +0200

    lesson 32-33

--- src/com/walking/lesson32_files_1/task2/Main.java
+++ src/com/walking/lesson32_files_1/task2/Main.java
@@ -1,10 +1,41 @@
 package com.walking.lesson32_files_1.task2;
 
+import com.walking.lesson32_files_1.task1.model.Car;
+import com.walking.lesson32_files_1.task2.parser.CarParser;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
 /**
  * Используя Задачу 1, реализуйте чтение из carCatalog.txt,
  * реализовав сохранение данных в массив Car.
  */
 public class Main {
     public static void main(String[] args) {
+
+        String carsString = readCars("./carCatalog.txt");
+
+        Car[] cars = CarParser.parseToCar(carsString);
+
+        for (Car car : cars) {
+            if (car != null) {
+                System.out.println(car);
+            }
+        }
+    }
+
+    static String readCars(String path) {
+        StringBuilder cars = new StringBuilder();
+
+        try (FileInputStream inputStream = new FileInputStream(path)) {
+            int in;
+            while ((in = inputStream.read()) != -1) {
+                cars.append((char) in);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return cars.toString();
     }
 }
--- src/com/walking/lesson33_files_2/task2/Main.java
+++ src/com/walking/lesson33_files_2/task2/Main.java
@@ -1,5 +1,13 @@
 package com.walking.lesson33_files_2.task2;
 
+import com.walking.lesson32_files_1.task1.model.Car;
+import com.walking.lesson32_files_1.task2.parser.CarParser;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Реализуйте Задачу 2 из урока
  * <a href="https://telegra.ph/Rabota-s-fajlami-CHast-I-12-17">...</a>
@@ -11,5 +19,29 @@ package com.walking.lesson33_files_2.task2;
  */
 public class Main {
     public static void main(String[] args) {
+
+        File carCatalog = new File("./", "carCatalog.txt");
+        Car[] cars = CarParser.parseToCar(readCars(carCatalog));
+
+        for (Car car : cars) {
+            if (car != null) {
+                System.out.println(car);
+            }
+        }
+    }
+
+    static String readCars(File file) {
+        StringBuilder cars = new StringBuilder();
+
+        try (FileInputStream inputStream = new FileInputStream(file)) {
+            int in;
+            while ((in = inputStream.read()) != -1) {
+                cars.append((char) in);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return cars.toString();
     }
 }
