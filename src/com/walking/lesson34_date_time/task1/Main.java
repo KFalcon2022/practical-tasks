4e8f72e521ceee36fbadea1a104dc86ed40e2893
Zawadskiy Serhii <srgzavadsky@gmail.com>
2023-01-11 17:22:06 +0200

    lesson 34 1,2

--- src/com/walking/lesson34_date_time/task1/Main.java
+++ src/com/walking/lesson34_date_time/task1/Main.java
@@ -1,10 +1,28 @@
 package com.walking.lesson34_date_time.task1;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+import java.util.Scanner;
+
 /**
  * Реализуйте метод, возвращающий объект LocalDateTime на основании переданной строки.
  * Пример входящей строки: 20.12.2022 20:16:00.
  */
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Spell the date in format dd.MM.yyyy HH:mm:ss");
+
+        String dateTime = scanner.nextLine();
+
+        LocalDateTime localDateTime = LocalDateTime.parse(dateTime, new DateTimeFormatterBuilder()
+                .appendPattern("dd.MM.yyyy HH:mm:ss")
+                .parseDefaulting(ChronoField.NANO_OF_SECOND, 0)
+                .toFormatter());
+        scanner.close();
     }
 }
