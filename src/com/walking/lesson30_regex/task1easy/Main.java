a441cea8e8f52ddc55c61c92a744966c130bcfa5
SergioZ14 <sergioz140592@gmail.com>
2023-01-20 15:36:43 +0300

    Done

---
+++ src/com/walking/lesson30_regex/task1easy/Main.java
@@ -0,0 +1,24 @@
+package com.walking.lesson30_regex.task1easy;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number:");
+        String number1 = scanner.nextLine();
+        scanner.close();
+
+        System.out.println(isCorrectNumber(number1));
+    }
+
+    public static boolean isCorrectNumber(String number) {
+        Pattern pattern = Pattern.compile("\\+\\d{1,3}\\s\\(\\d{3}\\)\s\\d{3}-\\d{2}-\\d{2}");
+
+        Matcher matcher = pattern.matcher(number);
+
+        return matcher.matches();
+    }
+}
