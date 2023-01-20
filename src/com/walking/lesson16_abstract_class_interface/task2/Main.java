b029e4c3a5559286788c4a50c949aea81491576e
mar4ehk0 <mar4ehk0@gmail.com>
2023-01-20 21:03:42 +0300

    Adds task2

--- src/com/walking/lesson16_abstract_class_interface/task2/Main.java
+++ src/com/walking/lesson16_abstract_class_interface/task2/Main.java
@@ -1,5 +1,12 @@
 package com.walking.lesson16_abstract_class_interface.task2;
 
+import com.walking.lesson16_abstract_class_interface.task2.model.GoodBye;
+import com.walking.lesson16_abstract_class_interface.task2.model.Hello;
+import com.walking.lesson16_abstract_class_interface.task2.model.HowAU;
+import com.walking.lesson16_abstract_class_interface.task2.model.Message;
+
+import java.util.Scanner;
+
 /**
  * Реализуйте задачу
  * <a href="https://github.com/KFalcon2022/practical-tasks/blob/master/src/com/walking/lesson3/Task2SwitchCase.java">...</a>
@@ -7,5 +14,30 @@ package com.walking.lesson16_abstract_class_interface.task2;
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+
+            System.out.print("Enter string: ");
+            String s = scanner.nextLine();
+            scanner.close();
+
+            Message message = getAnswer(s);
+            System.out.println(message.answer());
+        } catch (Throwable throwable) {
+            System.out.println(throwable.getMessage());
+        }
+    }
+
+    private static Message getAnswer(String s) throws Exception {
+        switch (s) {
+            case "Hi":
+                return new Hello();
+            case "Bye":
+                return new GoodBye();
+            case "How are you":
+                return new HowAU();
+            default:
+                throw new Exception("I don't answer");
+        }
     }
 }
---
+++ src/com/walking/lesson16_abstract_class_interface/task2/model/HowAU.java
@@ -0,0 +1,8 @@
+package com.walking.lesson16_abstract_class_interface.task2.model;
+
+public class HowAU implements Message{
+    @Override
+    public String answer() {
+        return "How are your doing";
+    }
+}
