6b0d08934e5d04b580c1867acd56ad711afe00ac
Yahor <egor.2000.xaxaxa@gmail.com>
2023-01-21 17:24:43 +0400

    Lessons 37-40

---
+++ src/com/walking/lesson39_queue1/task1/Main.java
@@ -0,0 +1,15 @@
+package com.walking.lesson39_queue1.task1;
+
+/**
+ * Реализуйте односвязный список (можно использовать реализацию в рамках
+ * <a href="https://github.com/KFalcon2022/practical-tasks/blob/master/src/com/walking/lesson28_generics1/task4/structure/Stack.java">...</a>).
+ * <p>
+ * Реализуйте метод, разворачивающий односвязный список
+ * (первый элемент должен стать последним, второй – предпоследним и т.д.).
+ * <p>
+ * Также реализуйте метод, удаляющий все узлы, хэшкод элемента которых - четный.
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
