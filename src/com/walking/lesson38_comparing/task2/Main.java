18d68c737bb6adff5afb576c1368e59f1ec837c3
Yahor <egor.2000.xaxaxa@gmail.com>
2023-01-21 17:24:43 +0400

    Lessons 37-40

---
+++ src/com/walking/lesson38_comparing/task2/Main.java
@@ -0,0 +1,15 @@
+package com.walking.lesson38_comparing.task2;
+
+/**
+ * Используя классы-сущности из задачи
+ * <a href="https://github.com/KFalcon2022/practical-tasks/tree/master/src/com/walking/lesson21_immutable_object/model">...</a>,
+ * имплементируйте Comparable для Car таким образом,
+ * чтобы машины сравнивались по полю identifier.
+ * Объекты CarIdentifier предлагаю сравнивать по номеру и году (если номера почему-то совпали).
+ * <p>
+ * Также реализуйте сортировку коллекции машин в прямом и обратном порядке.
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
