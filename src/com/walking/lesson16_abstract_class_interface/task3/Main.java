1a988e02988682735802f7c19c39a207f8713c25
mar4ehk0 <mar4ehk0@gmail.com>
2023-01-25 20:25:50 +0300

    implements lesson 17

--- src/com/walking/lesson16_abstract_class_interface/task3/Main.java
+++ src/com/walking/lesson16_abstract_class_interface/task3/Main.java
@@ -1,11 +1,6 @@
 package com.walking.lesson16_abstract_class_interface.task3;
 
 
-import com.walking.lesson16_abstract_class_interface.task3.model.Animal;
-import com.walking.lesson16_abstract_class_interface.task3.model.Cat;
-import com.walking.lesson16_abstract_class_interface.task3.model.Cow;
-import com.walking.lesson16_abstract_class_interface.task3.model.Dog;
-
 /**
  * Реализуйте любую задачу из уроков о наследовании или полиморфизме с использованием новых знаний.
  * Выбирайте инструмент с умом.
@@ -15,14 +10,5 @@ import com.walking.lesson16_abstract_class_interface.task3.model.Dog;
  */
 public class Main {
     public static void main(String[] args) {
-        Animal[] animals = new Animal[]{new Cat(), new Dog(), new Cow()};
-
-        soundAll(animals);
-    }
-
-    private static void soundAll(Animal[] animals) {
-        for (Animal animal : animals) {
-            animal.sound();
-        }
     }
 }
