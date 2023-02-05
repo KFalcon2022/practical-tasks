9cd9f32eca54c7aa3d163b02d606f791caef34c9
Yahor <egor.2000.xaxaxa@gmail.com>
2023-02-05 16:28:21 +0400

    Lessons 41-43

---
+++ src/com/walking/lesson41_set/task1/Main.java
@@ -0,0 +1,13 @@
+package com.walking.lesson41_set.task1;
+
+/**
+ * Реализуйте задачу
+ * <a href="https://github.com/KFalcon2022/practical-tasks/tree/master/src/com/walking/lesson40_queue2">...</a>,
+ * используя Set, вместо Queue.
+ * Продумайте, как можно эффективно реализовать задачу,
+ * чтобы сохранить принцип FIFO при обработке заданий.
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
