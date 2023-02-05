5b72ae0c112e1894941266f240e346514e30de72
Yahor <egor.2000.xaxaxa@gmail.com>
2023-02-05 16:28:21 +0400

    Lessons 41-43

---
+++ src/com/walking/lesson43_map/task1/Main.java
@@ -0,0 +1,13 @@
+package com.walking.lesson43_map.task1;
+
+/**
+ * Реализуйте программу, выводящую в консоль количество использований каждого из уникальных слов
+ * во введенной пользователем строке.
+ * <p>
+ * За основу предлагаю взять реализацию из задачи
+ * <a href="https://github.com/KFalcon2022/practical-tasks/tree/master/src/com/walking/lesson30_regex/task3">...</a>.
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/com/walking/lesson43_map/task2/Main.java
@@ -0,0 +1,12 @@
+package com.walking.lesson43_map.task2;
+
+/**
+ * Реализуйте задачу
+ * <a href="https://github.com/KFalcon2022/practical-tasks/tree/master/src/com/walking/lesson19_object_methods">...</a>,
+ * используя Map.
+ * Реализацию выберите исходя из особенностей исходной задачи.
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
