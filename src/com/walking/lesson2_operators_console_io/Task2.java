ad567bcb442099fd32f6c8a50faa966da23b42a5
Brillionb <welkommen@yandex.ru>
2023-02-08 13:52:41 +0300

    Lesson 2 - Task 1, 2, 3

--- src/com/walking/lesson2_operators_console_io/Task2.java
+++ src/com/walking/lesson2_operators_console_io/Task2.java
@@ -1,5 +1,7 @@
 package com.walking.lesson2_operators_console_io;
 
+import java.util.Scanner;
+
 /**
  * Введите с клавиатуры целые числа a и b.
  * Выведите на экран результат сравнения:
@@ -7,5 +9,11 @@ package com.walking.lesson2_operators_console_io;
  */
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите два целых числа по порядку: ");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        boolean c = (a * a * a) > (b * b);
+        System.out.println(c);
     }
 }
