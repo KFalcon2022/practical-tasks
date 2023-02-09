3df1f5b57981eac5c3ee85b8009d050fbb118c8e
HDefender <58877461+HDefender@users.noreply.github.com>
2023-02-09 21:50:50 +0200

    Update Main.java

--- src/com/walking/lesson8_classes_objects/Main.java
+++ src/com/walking/lesson8_classes_objects/Main.java
@@ -14,23 +14,23 @@ package com.walking.lesson8_classes_objects;
  * Конечные показатели счетчиков вывести в консоль.
  */
 public class Main {
-    public static void main(String[] args) {
-        Counter evenCounter = new Counter("Четные числа");
-        Counter oddCounter = new Counter("Нечетные числа");
+       public static void main(String[] args) {
+        Counter calcHonest = new Counter("Wall", 0);
+        Counter calcOdd = new Counter("Brick", 0);
 
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 0; i < 100; i++) {
             if (i % 2 == 0) {
-                evenCounter.increment();
-            } else {
-                oddCounter.increment();
-            }
+                calcHonest.counter++;
+            } else
+                calcOdd.counter ++;
         }
 
-        printCounterValue(evenCounter);
-        printCounterValue(oddCounter);
-    }
+        System.out.println("Число четных чисел "+ calcHonest.counter);
+        System.out.println("Число нечетных чисел "+ calcOdd.counter);
 
-    static void printCounterValue(Counter counter) {
-        System.out.printf("%s: %d\n", counter.name, counter.counter);
-    }
+        calcHonest.incrimentOne();
+
+        System.out.println("Число четных чисел "+ calcHonest.counter);
+        System.out.println("Число нечетных чисел "+ calcOdd.counter);
+        }
 }
