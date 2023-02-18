6758b556aeb12fdc6491baa9b291a3c529011a5f
Yahor <egor.2000.xaxaxa@gmail.com>
2023-02-18 20:43:51 +0400

    Lessons 46-49

---
+++ src/com/walking/lesson47_method_reference/task3/Main.java
@@ -0,0 +1,12 @@
+package com.walking.lesson47_method_reference.task3;
+
+/**
+ * Реализуйте Задачу 3 из урока 46,
+ * описав все реализуемые фильтры через method reference’ы.
+ * Рекомендую вынести функциональность формирования фильтров в отдельный сервис,
+ * если это не было сделано ранее.
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
