008f4813fea39eaf0470a1f3573b1dcc727c5bc7
Yahor <egor.2000.xaxaxa@gmail.com>
2023-02-18 20:43:51 +0400

    Lessons 46-49

---
+++ src/com/walking/lesson47_method_reference/task2/Main.java
@@ -0,0 +1,10 @@
+package com.walking.lesson47_method_reference.task2;
+
+/**
+ * Реализуйте Задачу 1, обернув метод выведения записи в консоль
+ * (System.out.println()) в собственный статический метод.
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
