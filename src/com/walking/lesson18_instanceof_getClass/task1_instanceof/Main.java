c23b86ebbbf4f225e387f824b8b578448b747fbe
maestro125rus <54302862+maestro125rus@users.noreply.github.com>
2023-02-20 23:22:55 +0300

    Fixed tasks in task 3 & enums in task 1 and 2

--- src/com/walking/lesson18_instanceof_getClass/task1_instanceof/Main.java
+++ src/com/walking/lesson18_instanceof_getClass/task1_instanceof/Main.java
@@ -1,4 +1,8 @@
 package com.walking.lesson18_instanceof_getClass.task1_instanceof;
+import com.walking.lesson18_instanceof_getClass.task1_instanceof.model.Animal;
+import com.walking.lesson18_instanceof_getClass.task1_instanceof.model.Cat;
+import com.walking.lesson18_instanceof_getClass.task1_instanceof.model.Cow;
+import com.walking.lesson18_instanceof_getClass.task1_instanceof.model.Dog;
 
 /**
  * Реализовать задачу
@@ -6,6 +10,31 @@ package com.walking.lesson18_instanceof_getClass.task1_instanceof;
  * используя instanceof.
  */
 public class Main {
-    public static void main(String[] args) {
-    }
+	public static void main(String[] args) {
+		Animal[] animals = new Animal[]{new Cat(), new Dog(), new Cow()};
+
+		soundAll(animals);
+
+	}
+
+	private static void soundAll(Animal[] animals) {
+		for (Animal animal : animals) {
+			sound(animal);
+
+		}
+	}
+	
+
+	//    Используя поле класса Animal, определяем, какой настоящий класс обрабатываемого объекта
+	private static void sound(Animal animal) {
+		if (animal instanceof Cat){
+			((Cat) animal).meow();
+		} else if (animal instanceof Dog) {
+			((Dog) animal).woof();
+		} else if (animal instanceof Cow) {
+			((Cow) animal).moo();
+		}else {
+			System.out.println("Unknown animal!");
+		}
+	}
 }
