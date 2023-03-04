954993f785d918e284bdd152838a5f02b0b957c2
Yahor <egor.2000.xaxaxa@gmail.com>
2023-03-05 00:01:05 +0400

    Lessons 51-52

---
+++ src/com/walking/lesson52_stream_creation/task3_flatMap/Main.java
@@ -0,0 +1,13 @@
+package com.walking.lesson52_stream_creation.task3_flatMap;
+
+/**
+ * Реализуйте метод, возвращающий Stream<String> из дат месяца,
+ * номер которого был передан параметром.
+ * Выведите все даты года в консоль.
+ * Избегайте дублирования кода.
+ * В main() Stream должен быть лишь 1. Требует использования flatMap().
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
