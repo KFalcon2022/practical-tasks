53d67467d817f5c5eadfd4a35dfd78f9bb098677
maestro125rus <54302862+maestro125rus@users.noreply.github.com>
2023-03-12 05:25:20 +0300

    28.2 - Done

--- src/com/walking/lesson28_generics1/task2/Main.java
+++ src/com/walking/lesson28_generics1/task2/Main.java
@@ -1,10 +1,20 @@
 package com.walking.lesson28_generics1.task2;
 
+import com.walking.lesson28_generics1.task2.model.Generic;
+
 /**
  * Создать класс-обертку над объектом любого типа.
  * Предусмотреть boolean-метод, проверяющий значение объекта на null.
  */
 public class Main {
     public static void main(String[] args) {
+        Integer a = null;
+        String b = " ";
+
+        Generic <Integer> intGen = new Generic<>(a);
+        Generic <String> strGen = new Generic<>(b);
+
+        System.out.println(intGen.nullCheck(intGen.getObj()));
+        System.out.println(strGen.nullCheck(strGen.getObj()));
     }
 }
