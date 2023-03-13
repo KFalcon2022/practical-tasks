be53497dbfefd4e53caba1777252ed131ab2c455
maestro125rus <54302862+maestro125rus@users.noreply.github.com>
2023-03-13 21:31:08 +0300

    28.4 - done

--- src/com/walking/lesson28_generics1/task4/Main.java
+++ src/com/walking/lesson28_generics1/task4/Main.java
@@ -1,5 +1,7 @@
 package com.walking.lesson28_generics1.task4;
 
+import com.walking.lesson28_generics1.task4.exceptions.ElementNotFoundException;
+
 /**
  * Реализовать параметризованный класс, хранящий и обрабатывающий стек.
  * Стек — структура данных, в котором каждый элемент хранит ссылку на следующий.
@@ -15,6 +17,29 @@ package com.walking.lesson28_generics1.task4;
  * ([строковое представление элемента1, ..., строковое представление элементаN]).
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ElementNotFoundException {
+        StackService<String> nameList = new StackService<>();
+        nameList.addElement("Vasya");
+        nameList.addElement("Petya");
+        nameList.addElement("Anton");
+        nameList.addElement("Ahmed");
+        nameList.addElement("Oleg");
+
+        try {
+            System.out.println("Element is found!: " + nameList.searchElement("Oleg"));
+        }catch (ElementNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            nameList.removeElement("Anton");
+        }catch (ElementNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("List length: " + nameList.getLength() + " elements");
+
+        System.out.println(nameList);
+
     }
 }
