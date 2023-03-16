1960bd83de5ce3c5308748f1c43ccba06037e4f2
mcpopoff <mcpopoff@ya.ru>
2023-03-16 06:50:47 +0500

    16-17

---
+++ src/com/walking/lesson18_instanceof_getClass/Task1/model/Dog.java
@@ -0,0 +1,13 @@
+package com.walking.lesson18_instanceof_getClass.Task1.model;
+
+public class Dog extends Animal {
+    public static final String DOG_CLASS_NAME = "Dog";
+
+    public Dog() {
+        super(DOG_CLASS_NAME, "woof");
+    }
+
+    public void woof() {
+        sound();
+    }
+}
