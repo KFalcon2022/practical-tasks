e74833b69fe0d72f1c70a22037e903acab8284f3
HDefender <58877461+HDefender@users.noreply.github.com>
2023-03-22 22:42:48 +0300

    Update Main.java

--- src/com/walking/lesson16_abstract_class_interface/task3/Main.java
+++ src/com/walking/lesson16_abstract_class_interface/task3/Main.java
@@ -1,14 +1,17 @@
 package com.walking.lesson16_abstract_class_interface.task3;
 
 
-/**
- * Реализуйте любую задачу из уроков о наследовании или полиморфизме с использованием новых знаний.
- * Выбирайте инструмент с умом.
- * <p>
- * В качестве задачи выбрана
- * <a href="https://github.com/KFalcon2022/practical-tasks/tree/master/src/com/walking/lesson14_polymorphism/task2">...</a>
- */
 public class Main {
     public static void main(String[] args) {
+        Animal[] animals = new Animal[]{new Dog(), new Cow(), new Cat()};
+
+        soundAll(animals);
+
+    }
+
+    public static void soundAll(Animal[] animals) {
+        for (Animal animal : animals) {
+            animal.sound();
+        }
     }
 }
