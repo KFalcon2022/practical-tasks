b105fdd4175e3a2d938ea653a0840e09d4b424ed
maestro125rus <54302862+maestro125rus@users.noreply.github.com>
2023-04-01 16:49:35 +0300

    32.1 - Done 32.2 - Done

--- src/com/walking/lesson32_files_1/task1/Main.java
+++ src/com/walking/lesson32_files_1/task1/Main.java
@@ -33,15 +33,15 @@ public class Main {
 
     private static Car[] initCars() {
         Car car1 = new Car("RR-111-RR", 2015, "yellow", true);
-        Car car2 = new Car("RR-222-RR", 2016, "yellow", true);
-        Car car3 = new Car("RR-333-RR", 2017, "yellow", true);
-        Car car4 = new Car("RR-444-RR", 2018, "yellow", true);
-        Car car5 = new Car("RR-555-RR", 2018, "yellow", true);
-        Car car6 = new Car("RR-666-RR", 2018, "yellow", true);
+        Car car2 = new Car("RR-222-RR", 2016, "purple", true);
+        Car car3 = new Car("RR-333-RR", 2017, "grey", true);
+        Car car4 = new Car("RR-444-RR", 2018, "white", true);
+        Car car5 = new Car("RR-555-RR", 2018, "green", true);
+        Car car6 = new Car("RR-666-RR", 2018, "black", true);
         Car car7 = new Car("RR-777-RR", 2018, "yellow", true);
-        Car car8 = new Car("RR-888-RR", 2018, "yellow", true);
-        Car car9 = new Car("RR-999-RR", 2018, "yellow", true);
-        Car car10 = new Car("RR-000-RR", 2018, "yellow", true);
+        Car car8 = new Car("RR-888-RR", 2018, "brown", true);
+        Car car9 = new Car("RR-999-RR", 2018, "red", true);
+        Car car10 = new Car("RR-000-RR", 2018, "blue", true);
 
         return new Car[]{car1, car2, car3, car4, car5, car6, car7, car8, car9, car10};
     }
--- src/com/walking/lesson32_files_1/task2/Main.java
+++ src/com/walking/lesson32_files_1/task2/Main.java
@@ -1,10 +1,72 @@
 package com.walking.lesson32_files_1.task2;
 
+import com.walking.lesson32_files_1.task1.model.Car;
+
+import java.io.*;
+import java.util.Arrays;
+
 /**
  * Используя Задачу 1, реализуйте чтение из carCatalog.txt,
  * реализовав сохранение данных в массив Car.
  */
 public class Main {
     public static void main(String[] args) {
+
+        StringBuilder inputData = new StringBuilder();
+
+        try (FileInputStream fin = new FileInputStream("./resources/carCatalog.txt")) {
+            int i;
+            while ((i = fin.read()) != -1) {
+                inputData.append((char) i);
+            }
+        } catch (IOException ex) {
+
+            System.out.println(ex.getMessage());
+        }
+
+        int splitCounter = 0;
+        for (int i = 0; i < inputData.length() - 1; i++) {
+            if (inputData.charAt(i) == 10 && inputData.charAt(i + 1) == 10) {
+                inputData.deleteCharAt(i + 1);
+                splitCounter++;
+            }
+        }
+
+        Car[] cars = new Car[splitCounter];
+        String[] str = parseData(inputData);
+
+        for (int i = 0; i < cars.length; i++){
+            cars[i] = createNewCar(str);
+            String[] str1 = new String[str.length - 4];
+
+            for (int j = 0, k = 4; j < str.length - 4; j++, k++){
+                str1[j] = str[k];
+            }
+            str = str1;
+
+        }
+        System.out.println(Arrays.toString(cars));
+    }
+
+    private static String[] parseData (StringBuilder data){
+
+        String[] subStr = data.toString().split(".*: ");
+        StringBuilder str = new StringBuilder();
+
+        for (int i = 0; i < subStr.length; i++) {
+            str.append(subStr[i]);
+        }
+
+        return str.toString().split("\n");
+    }
+
+    private static Car createNewCar (String[] data){
+        String number = data[0];
+        int year = Integer.parseInt(data[1]);
+        String color = data[2];
+        boolean actualTechnicalInspection = data[3].equals("actual");
+
+
+        return new Car(number, year, color, actualTechnicalInspection);
     }
 }
