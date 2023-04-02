4942c0e824e710b04d501ddbef28c258f7ce74e5
Yahor <egor.2000.xaxaxa@gmail.com>
2023-04-02 15:28:59 +0400

    Lessons 58-62

---
+++ src/com/walking/lesson60_thread/task1/Main.java
@@ -0,0 +1,10 @@
+package com.walking.lesson60_thread.task1;
+
+/**
+ * Напишите программу, которая пишет в консоль текущее время каждый две секунды,
+ * пока программа запущена.
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
