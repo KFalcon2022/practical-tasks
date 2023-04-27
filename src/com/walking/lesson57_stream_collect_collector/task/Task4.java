b58a291409f50cd75b1aa1ec67c382b13c9cf8be
SergioZ14 <sergioz140592@gmail.com>
2023-04-27 13:41:41 +0300

    Done

--- src/com/walking/lesson57_stream_collect_collector/task/Task4.java
+++ src/com/walking/lesson57_stream_collect_collector/task/Task4.java
@@ -2,6 +2,7 @@ package com.walking.lesson57_stream_collect_collector.task;
 
 import com.walking.lesson57_stream_collect_collector.model.Department;
 
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,7 +14,7 @@ public class Task4 implements StatisticTask<LinkedList<Department>> {
     @Override
     public LinkedList<Department> calculate(List<Department> departments) {
         return departments.stream()
-                .sorted((object1, object2) -> object1.getName().compareTo(object2.getName()))
+                .sorted(Comparator.comparing(Department::getName))
                 .collect(Collectors.toCollection(LinkedList::new));
     }
 }
