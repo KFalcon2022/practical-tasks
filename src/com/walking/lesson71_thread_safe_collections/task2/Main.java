29fd919957ff7a98f5c23a25f71acb0794eb7487
Yahor <egor.2000.xaxaxa@gmail.com>
2023-05-01 20:14:19 +0400

    Lessons 68, 71

---
+++ src/com/walking/lesson71_thread_safe_collections/task2/Main.java
@@ -0,0 +1,15 @@
+package com.walking.lesson71_thread_safe_collections.task2;
+
+/**
+ * Создайте стрим на базе ArrayList и внутри стрима удаляйте элементы исходного списка.
+ * Например, пусть каждый элемент удаляет сам себя.
+ * <p>
+ * Попробуйте сделать то же самое, но заменив ArrayList на CopyOnWriteArrayList.
+ * Как изменился результат?
+ * <p>
+ * Можете попробовать также и с другими потокобезопасными и потоконебезопасными коллекциями.
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
