93600f69ad518ef2ecde4f8c82af083b2b6646e6
maestro125rus <54302862+maestro125rus@users.noreply.github.com>
2023-05-01 03:34:09 +0300

    39.1 - Done

--- src/com/walking/lesson39_queue1/task1/Main.java
+++ src/com/walking/lesson39_queue1/task1/Main.java
@@ -1,5 +1,8 @@
 package com.walking.lesson39_queue1.task1;
 
+import com.walking.lesson39_queue1.task1.model.SingleLinkedList;
+
+
 /**
  * Реализуйте односвязный список (можно использовать реализацию в рамках
  * <a href="https://github.com/KFalcon2022/practical-tasks/blob/master/src/com/walking/lesson28_generics1/task4/structure/Stack.java">...</a>).
@@ -10,6 +13,24 @@ package com.walking.lesson39_queue1.task1;
  * Также реализуйте метод, удаляющий все узлы, хэшкод элемента которых - четный.
  */
 public class Main {
-    public static void main(String[] args) {
-    }
+	public static void main(String[] args) {
+
+		SingleLinkedList<String> list = new SingleLinkedList<>();
+		list.add("bvsdsdg");
+		list.add("uignmf");
+		list.add("yufdn");
+		list.add("hgfghf");
+		list.add("ewqwewq");
+		list.add("zxczxc");
+		list.add("qweqwe");
+		list.add("asdasd");
+
+		System.out.println(list);
+		System.out.println(list.hashAllNodes());
+		list.removeEvenHashNodes();
+		System.out.println(list.hashAllNodes());
+		list.reverse();
+		System.out.println(list);
+
+	}
 }
