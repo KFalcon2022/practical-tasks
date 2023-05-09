eb90708b6d92cbcd9e855b45892404209580cddd
HDefender <58877461+HDefender@users.noreply.github.com>
2023-05-09 22:18:11 +0300

    Update Main.java

--- src/com/walking/lesson18_instanceof_getClass/task1_instanceof/Main.java
+++ src/com/walking/lesson18_instanceof_getClass/task1_instanceof/Main.java
@@ -1,17 +1,11 @@
-package com.walking.lesson18;
+package com.walking.lesson18.instance_of;
 
-/**
- * Реализовать задачу
- * <a href="https://github.com/KFalcon2022/practical-tasks/tree/master/src/com/walking/lesson13_inheritance/task2">...</a>
- * используя instanceof.
- */
 public class Main {
     public static void main(String[] args) {
         Animal[] animals = new Animal[]{new Cat(), new Dog(), new Cow()};
 
         soundAll(animals);
     }
-
     private static void soundAll(Animal[] animals) {
         for (Animal animal : animals) {
             sound(animal);
@@ -20,25 +14,22 @@ public class Main {
 
     //    Используя поле класса Animal, определяем, какой настоящий класс обрабатываемого объекта
     private static void sound(Animal animal) {
-        switch (animal.getClassName()) {
-            case Cat.CAT_CLASS_NAME:
-                if (animal instanceof Cat catty){ //проверяем, может ли животное стать котом с помощью оператора instanceof
-                    catty.meow(); // если может, то тогда кот мяукает
-                }
-                break;
-            case Dog.DOG_CLASS_NAME:
-                if (animal instanceof Dog doggo){ //проверяем, может ли животное стать собакой с помощью оператора instanceof
-                    doggo.woof(); // если может, то тогда собака гавкает
-                }
-                break;
-            case Cow.COW_CLASS_NAME:
-                if (animal instanceof Cow cowwo){ //проверяем, может ли животное стать коровой с помощью оператора instanceof
-                    cowwo.moo(); // если может, то тогда корова мычит
-                }
-                break;
-            default: //На случай, если был создан объект Animal, а не наследника.
+        if (animal instanceof Cat cat) { //проверяем, может ли животное стать котом с помощью оператора instanceof
+            cat.meow(); // если может, то тогда кот мяукает
+        }
+        else {
+            if (animal instanceof Dog dog) { //проверяем, может ли животное стать собакой с помощью оператора instanceof
+                dog.woof(); // если может, то тогда собака гавкает
+            }
+        else {
+            if (animal instanceof Cow cow) { //проверяем, может ли животное стать коровой с помощью оператора instanceof
+                cow.moo(); // если может, то тогда корова мычит
+            }
+        else {  //На случай, если был создан объект Animal, а не наследника.
                 // Или если наследник неизвестен
                 System.out.println("Unknown animal!");
+            }
+            }
         }
     }
 }
