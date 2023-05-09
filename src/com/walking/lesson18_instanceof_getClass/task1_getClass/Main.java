a0e2e1838c33c63c64b7cb7b77fbc2f8b8722591
HDefender <58877461+HDefender@users.noreply.github.com>
2023-05-09 22:17:05 +0300

    Update Main.java

--- src/com/walking/lesson18_instanceof_getClass/task1_getClass/Main.java
+++ src/com/walking/lesson18_instanceof_getClass/task1_getClass/Main.java
@@ -1,9 +1,24 @@
 package com.walking.lesson18.getClass;
 
 /**
- * Реализовать задачу
- * <a href="https://github.com/KFalcon2022/practical-tasks/tree/master/src/com/walking/lesson13_inheritance/task2">...</a>
- * используя getClass().
+ * Реализуйте класс Animal. Реализуйте его наследников: Dog, Cat, Cow.
+ * Каждый из наследников должен содержать свой метод: woof(), meow() и moo() соответственно.
+ * Остальные поля и методы суперкласса и наследников реализовать по своему усмотрению,
+ * если они необходимы.
+ * <p>
+ * Каждый из методов должен выводить в консоль соответствующую ему строку:
+ * "woof ", "meow " или "moo".
+ * <p>
+ * В main() создать и наполнить в произвольном порядке
+ * объектами разных классов-наследников массив типа Animal.
+ * Реализовать метод, принимающий массив Animal и вызывающий метод,
+ * характерный для конкретного животного.
+ * <p>
+ * Использовать при решении instanceof, getClass() или другие неизвестные нам механики – недопустимо.
+ * <p>
+ * Дополнительное условие (необязательно): решить задачу, при условии,
+ * что woof(), meow() и moo() внутри себя выполняют только вызов protected-метода sound(),
+ * который определен в Animal.
  */
 public class Main {
     public static void main(String[] args) {
@@ -11,9 +26,7 @@ public class Main {
 
         soundAll(animals);
     }
-
     private static void soundAll(Animal[] animals) {
-
         for (Animal animal : animals) {
             sound(animal);
         }
@@ -21,25 +34,23 @@ public class Main {
 
     //    Используя поле класса Animal, определяем, какой настоящий класс обрабатываемого объекта
    private static void sound(Animal animal) {
-        switch (animal.getClassName()) {
-            case Cat.CAT_CLASS_NAME:
-                if(animal.getClass().equals(Cat.class)) { //проверка на тип объекта, является ли тип переменной Cat и Animal одинаковым
-                    ((Cat) animal).meow();
-                }
-                break;
-            case Dog.DOG_CLASS_NAME:
-                if(animal.getClass().equals(Cat.class)){ //проверка на тип объекта, является ли тип переменной Dog и Animal одинаковым
+           if(Cat.class.equals(animal.getClass())) { //проверка на тип объекта, является ли тип переменной Cat и Animal одинаковым
+                  ((Cat) animal).meow();
+           }
+           else {
+               if(Dog.class.equals(animal.getClass())){ //проверка на тип объекта, является ли тип переменной Dog и Animal одинаковым
                    ((Dog)animal).woof();
                 }
-                break;
-            case Cow.COW_CLASS_NAME:
-                if(animal.getClass().equals(Cat.class)){ //проверка на тип объекта, является ли тип переменной Cow и Animal одинаковым
-                    ((Cow)animal).moo();
+           else {
+               if (Cow.class.equals(animal.getClass())) { //проверка на тип объекта, является ли тип переменной Cow и Animal одинаковым
+                   ((Cow) animal).moo();
+               }
+           else {
+                   //На случай, если был создан объект Animal, а не наследника.
+                   // Или если наследник неизвестен
+                   System.out.println("Unknown animal!");
                 }
-                break;
-            default: //На случай, если был создан объект Animal, а не наследника.
-                // Или если наследник неизвестен
-                System.out.println("Unknown animal!");
+           }
         }
     }
 }
