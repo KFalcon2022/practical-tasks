f5123dc8d155fcb6649e9a5085a8fa4ef342c235
”Denis <eduhomeowl@gmail.com>
2023-05-15 20:02:00 +0300

    Solution of Task 1 lesson 16

---
+++ src/com/walking/lesson16_abstract_class_interface/task1_abstract_class/model/EquilateralShape.java
@@ -0,0 +1,19 @@
+package com.walking.lesson16_abstract_class_interface.task1_abstract_class.model;
+
+public abstract class EquilateralShape {
+    public static final String EMPTY_ELEMENT = " ";
+    public static final String EMPTY_STRING = "";
+    public static final char NEW_LINE_SYMBOL = '\n';
+
+    protected final int length;
+
+    protected EquilateralShape(int length) {
+        this.length = length;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public abstract String createShapeString() ;
+}
