d0d440fa224d42d696c42fb1b25b07e3bc1ded38
”Denis <eduhomeowl@gmail.com>
2023-05-15 20:47:07 +0300

    Solution of Task 3 lesson 16

--- src/com/walking/lesson16_abstract_class_interface/task3/Main.java
+++ src/com/walking/lesson16_abstract_class_interface/task3/Main.java
@@ -8,7 +8,25 @@ package com.walking.lesson16_abstract_class_interface.task3;
  * В качестве задачи выбрана
  * <a href="https://github.com/KFalcon2022/practical-tasks/tree/master/src/com/walking/lesson14_polymorphism/task2">...</a>
  */
+
+import com.walking.lesson16_abstract_class_interface.task3.model.*;
+
 public class Main {
     public static void main(String[] args) {
+        Animal[] animals = new Animal[3];
+
+        animals[0] = new Cat();
+        animals[1] = new Dog();
+        animals[2] = new Cow();
+
+        outAnimals(animals);
     }
+
+    private static void outAnimals(Animal[] animals) {
+        for (Animal animal : animals) {
+            animal.sound();
+        }
+    }
+
 }
+
---
+++ src/com/walking/lesson16_abstract_class_interface/task3/model/Animal.java
@@ -0,0 +1,6 @@
+package com.walking.lesson16_abstract_class_interface.task3.model;
+
+
+public abstract class Animal {
+    public abstract void sound();
+}
