e0ef85060930ca5c9e82d3f883b874f364c68795
jokend1ng2 <jokend1ng2@yandex.ru>
2023-05-16 21:44:03 +0300

    complete Homework 35

--- src/com/walking/lesson29_generics2/task2/Main.java
+++ src/com/walking/lesson29_generics2/task2/Main.java
@@ -2,6 +2,11 @@ package com.walking.lesson29_generics2.task2;
 
 import com.walking.lesson29_generics2.task2.model.NumberExponentiater;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 /**
  * Используя Задачу 1 из урока Generics. Часть I,
  * реализуйте в Main метод, принимающий аргументом объект подходящего
@@ -10,11 +15,124 @@ import com.walking.lesson29_generics2.task2.model.NumberExponentiater;
  */
 public class Main {
     public static void main(String[] args) {
-        NumberExponentiater<?> integerExponentiater = createNumberExponentiater(1);
-        System.out.println(integerExponentiater.getNumber());
-    }
+  List<?super Number> a = new List<>() {
+      @Override
+      public int size() {
+          return 0;
+      }
 
-    public static <T extends Number> NumberExponentiater<T> createNumberExponentiater(T number) {
-    return new NumberExponentiater<T>(number);
-    }
+      @Override
+      public boolean isEmpty() {
+          return false;
+      }
+
+      @Override
+      public boolean contains(Object o) {
+          return false;
+      }
+
+      @Override
+      public Iterator<Number> iterator() {
+          return null;
+      }
+
+      @Override
+      public Object[] toArray() {
+          return new Object[0];
+      }
+
+      @Override
+      public <T> T[] toArray(T[] a) {
+          return null;
+      }
+
+      @Override
+      public boolean add(Number number) {
+          return false;
+      }
+
+      @Override
+      public boolean remove(Object o) {
+          return false;
+      }
+
+      @Override
+      public boolean containsAll(Collection<?> c) {
+          return false;
+      }
+
+      @Override
+      public boolean addAll(Collection<? extends Number> c) {
+          return false;
+      }
+
+      @Override
+      public boolean addAll(int index, Collection<? extends Number> c) {
+          return false;
+      }
+
+      @Override
+      public boolean removeAll(Collection<?> c) {
+          return false;
+      }
+
+      @Override
+      public boolean retainAll(Collection<?> c) {
+          return false;
+      }
+
+      @Override
+      public void clear() {
+
+      }
+
+      @Override
+      public Number get(int index) {
+          return null;
+      }
+
+      @Override
+      public Number set(int index, Number element) {
+          return null;
+      }
+
+      @Override
+      public void add(int index, Number element) {
+
+      }
+
+      @Override
+      public Number remove(int index) {
+          return null;
+      }
+
+      @Override
+      public int indexOf(Object o) {
+          return 0;
+      }
+
+      @Override
+      public int lastIndexOf(Object o) {
+          return 0;
+      }
+
+      @Override
+      public ListIterator<Number> listIterator() {
+          return null;
+      }
+
+      @Override
+      public ListIterator<Number> listIterator(int index) {
+          return null;
+      }
+
+      @Override
+      public List<Number> subList(int fromIndex, int toIndex) {
+          return null;
+      }
+  };
+  a.add(2,2);
+  a.add(2,2.2);
+  a.add(2,2.2);
+}
 }
