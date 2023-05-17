6f7e5bd4b270392b26db64615b16096e8bcf79ef
maestro125rus <54302862+maestro125rus@users.noreply.github.com>
2023-05-17 19:54:07 +0300

    41,2 - Done

--- src/com/walking/lesson41_set/task2/Main.java
+++ src/com/walking/lesson41_set/task2/Main.java
@@ -1,5 +1,7 @@
 package com.walking.lesson41_set.task2;
 
+import java.util.*;
+
 /**
  * Реализуйте задачу
  * <a href="https://github.com/KFalcon2022/practical-tasks/tree/master/src/com/walking/lesson26_string_types/task2">...</a>,
@@ -7,5 +9,20 @@ package com.walking.lesson41_set.task2;
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter words: ");
+
+        String[] words = scanner.nextLine()
+                .trim() //обрезаем пробелы в начале и коне введенной строки
+                .toLowerCase() //приводим все к нижнему регистру, чтобы слова в разном регистре не посчитались по отдельности
+                .split(" "); //разбиваем строку на массив, разделенный по " "*/
+
+        scanner.close();
+
+        Set <String> uniqWords = new LinkedHashSet<>(List.of(words));
+
+        System.out.println(uniqWords.size());
+
     }
 }
