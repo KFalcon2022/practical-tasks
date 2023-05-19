47737e5de99540c61100aa6e830a3cc5388bc192
”Denis <eduhomeowl@gmail.com>
2023-05-19 15:57:52 +0300

    Solution of Task 2 lesson 25

--- src/com/walking/lesson25_nested_classes/task2/nested_class/Main.java
+++ src/com/walking/lesson25_nested_classes/task2/nested_class/Main.java
@@ -6,6 +6,46 @@ package com.walking.lesson25_nested_classes.task2.nested_class;
  * используя внутренние классы.
  */
 public class Main {
+
+    private class Animal {
+
+        protected Animal() {}
+
+        public void sound() {
+            System.out.println("Unknown animal!");
+        }
+    }
+
+    private class Cat extends Animal {
+        @Override
+        public void sound() {
+            System.out.println("meow");
+        }
+    }
+
+    private class Cow extends Animal {
+        @Override
+        public void sound() {
+            System.out.println("moo");
+        }
+    }
+
+    private class Dog extends Animal {
+        @Override
+        public void sound() {
+            System.out.println("woof");
+        }
+    }
     public static void main(String[] args) {
+        Main main = new Main();
+        Animal[] animals = new Animal[]{main.new Cat(), main.new Dog(), main.new Cow()};
+
+        soundAll(animals);
+    }
+
+    private static void soundAll(Animal[] animals) {
+        for (Animal animal : animals) {
+            animal.sound();
+        }
     }
 }
