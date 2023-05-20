22a23ae153cb4e22e363d60828662b885c1ab81a
”Denis <eduhomeowl@gmail.com>
2023-05-20 16:53:06 +0300

    Solution of Task 2 lesson 26

--- src/com/walking/lesson26_string_types/task2/Main.java
+++ src/com/walking/lesson26_string_types/task2/Main.java
@@ -10,7 +10,43 @@ package com.walking.lesson26_string_types.task2;
  * отличные от пробела или русских/английский букв.
  * Помните, что слово может быть введено в разных регистрах.
  */
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter string:");
+        String input = scanner.nextLine();
+        scanner.close();
+
+        String[] strings = input.split("\s");
+
+        for (int i=0; i < strings.length; i++ ) {
+            if (strings[i].isBlank()){
+                continue;
+            }
+            int count = 0;
+            int lastIdx = 0;
+
+            while (lastIdx != -1){
+                lastIdx = input.indexOf(strings[i],lastIdx);
+                if (lastIdx != -1){
+                    count++;
+                    lastIdx += lastIdx + strings[i].length();
+                }
+            }
+
+            System.out.printf("%s = %d\n", strings[i], count);
+
+            for (int j = i + 1; j < strings.length; j++){
+                if (strings[i].equals(strings[j])){
+                  strings[j] = "";
+                }
+            }
+        }
+//            System.out.println(string);
     }
+
 }
