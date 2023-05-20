a587fb733a42f272272d9305b8b2e30ba0aa48b6
”Denis <eduhomeowl@gmail.com>
2023-05-20 16:13:03 +0300

    Solution of Task 1 lesson 26

--- src/com/walking/lesson26_string_types/task1/Main.java
+++ src/com/walking/lesson26_string_types/task1/Main.java
@@ -4,8 +4,71 @@ package com.walking.lesson26_string_types.task1;
  * Реализуйте задачу
  * <a href="https://github.com/KFalcon2022/practical-tasks/blob/master/src/com/walking/lesson6_methods/Task3.java">...</a>
  * используя StringBuilder или StringBuffer. Объясните свой выбор.
+ * Написать программу, которая принимает длину и ширину прямоугольника (2 целых числа).
+ * Нарисовать в консоли заданный прямоугольник, используя “-“ и “|”.
+ * Углы прямоугольника обозначить символом “ “.
+ * Каждая единица длины должна обозначаться одним символом “-“,
+ * каждая единица ширины – символом “|“.
+ *
  */
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int length = requireInt(scanner, "Enter rectangle's length: ");
+        int width = requireInt(scanner, "Enter rectangle's width: ");
+
+        scanner.close();
+
+        StringBuilder horizontalLine = createHorizontalLine(length);
+        StringBuilder verticalLines = createVerticalLines(length, width);
+
+        printRectangle(horizontalLine, verticalLines);
     }
-}
+
+    static int requireInt(Scanner scanner, String requiringMessage) {
+        System.out.print(requiringMessage);
+
+        return scanner.nextInt();
+    }
+
+    static void printRectangle(StringBuilder horizontalLine, StringBuilder verticalLines) {
+        System.out.print(horizontalLine);
+        System.out.print(verticalLines);
+        System.out.print(horizontalLine);
+    }
+
+    static StringBuilder createHorizontalLine(int length) {
+        StringBuilder horizontalLine = new StringBuilder(" ");
+
+        for (int i = 0; i < length; i++) {
+            horizontalLine.append("-");
+        }
+
+        horizontalLine.append(" \n");
+
+        return horizontalLine;
+    }
+
+    static StringBuilder createVerticalLines(int length, int width) {
+        StringBuilder verticalLinesUnit = getVerticalLinesUnit(length);
+
+        StringBuilder verticalLines = new StringBuilder("");
+
+        for (int i = 0; i < width; i++) {
+            verticalLines.append(verticalLinesUnit);
+        }
+        return verticalLines;
+    }
+
+    static StringBuilder getVerticalLinesUnit(int length) {
+        StringBuilder verticalLinesUnit = new StringBuilder("|");
+
+        for (int i = 0; i < length; i++) {
+            verticalLinesUnit.append(" ");
+        }
+
+        verticalLinesUnit.append("|\n");
+        return verticalLinesUnit;
+    }}
