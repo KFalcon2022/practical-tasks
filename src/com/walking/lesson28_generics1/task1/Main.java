a47bb5caded53f3ecf30f868cbb115ab2aaad618
”Denis <eduhomeowl@gmail.com>
2023-05-21 11:05:33 +0300

    Solution of Task 1 lesson 28

--- src/com/walking/lesson28_generics1/task1/Main.java
+++ src/com/walking/lesson28_generics1/task1/Main.java
@@ -5,7 +5,15 @@ package com.walking.lesson28_generics1.task1;
  * Создать метод, возводящий значение параметризованного типа в степень,
  * переданную параметром в метод.
  */
+import com.walking.lesson28_generics1.task1.model.GenNumber;
 public class Main {
     public static void main(String[] args) {
+
+        GenNumber<Integer> integer = new GenNumber<>(10);
+        System.out.println(integer.calcPower(5));
+
+        GenNumber<Double> doubleNumber = new GenNumber<>(10d);
+        System.out.println(doubleNumber.calcPower(5));
+
     }
 }
