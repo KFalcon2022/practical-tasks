ed179fb1f69cdbd0b0413692beb5eea6e5fc5b37
”Denis <eduhomeowl@gmail.com>
2023-05-21 11:14:23 +0300

    Solution of Task 2 lesson 28

--- src/com/walking/lesson28_generics1/task2/Main.java
+++ src/com/walking/lesson28_generics1/task2/Main.java
@@ -4,7 +4,12 @@ package com.walking.lesson28_generics1.task2;
  * Создать класс-обертку над объектом любого типа.
  * Предусмотреть boolean-метод, проверяющий значение объекта на null.
  */
+import com.walking.lesson28_generics1.task2.model.ObjectNullComparator;
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println(new ObjectNullComparator<Integer>( new Integer(5)).isNull());
+        System.out.println(new ObjectNullComparator<Integer>( null ).isNull());
+
     }
 }
