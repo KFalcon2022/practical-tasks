dc2a879ab0c07d124993af262d3762ebf29883e7
”Denis <eduhomeowl@gmail.com>
2023-05-21 20:57:22 +0300

    Solution of Task 1 lesson 29

--- src/com/walking/lesson29_generics2/task1/Main.java
+++ src/com/walking/lesson29_generics2/task1/Main.java
@@ -8,7 +8,27 @@ package com.walking.lesson29_generics2.task1;
  * Метод должен возвращать значение поля дженерика, если оно != null,
  * в противном случае — возвращать второй параметр.
  */
+
+import com.walking.lesson29_generics2.task1.model.Store;
+
 public class Main {
     public static void main(String[] args) {
+
+        Store<Integer> storeInteger = new Store<>(32);
+        System.out.println(getGeneric(storeInteger,34));
+
+        Store<Integer> storeNull = new Store<>(null);
+        System.out.println(getGeneric(storeNull,34));
+
+    }
+
+    private static <T> T getGeneric(Store<T> store, T instance) {
+
+        if (store.getInstance() != null) {
+            return store.getInstance();
+        } else {
+            return instance;
+        }
+
     }
 }
