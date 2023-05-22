a99e33d3fa0b587a20c58f6d625c8393fac7887c
”Denis <eduhomeowl@gmail.com>
2023-05-22 15:21:55 +0300

    Solution of Task 1 lesson 30

--- src/com/walking/lesson30_regex/task1hard/Main.java
+++ src/com/walking/lesson30_regex/task1hard/Main.java
@@ -14,7 +14,19 @@ package com.walking.lesson30_regex.task1hard;
  * так и отсутствовать вовсе, возможность частичного использования
  * или использования символов-разделителей в другом порядке считаем невалидным)
  */
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter phone number:");
+        String phoneNumber = scanner.nextLine();
+        scanner.close();
+
+        if (phoneNumber.matches("^\\+7[-\\s]?(\\(\\d{3}\\)|\\d{3})[-\\s]?\\d{3}[-\\s]?\\d{2}[-\\s]?\\d{2}$")){
+            System.out.println("correct phone number");
+        } else {
+            System.out.println("wrong phone number");
+        }
     }
 }
