ecb31d83e705220c4804b11f4a9709440ffe8ea7
Yahor <egor.2000.xaxaxa@gmail.com>
2023-05-28 01:53:30 +0400

    Lesson 77

---
+++ src/com/walking/lesson77_future/task2/Main.java
@@ -0,0 +1,9 @@
+package com.walking.lesson77_future.task2;
+
+/**
+ * Реализуйте параллельный поиск максимального значения в массиве чисел.
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
