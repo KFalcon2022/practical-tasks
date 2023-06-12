de788bae7536b57123c527b521420290076840f9
maestro125rus <54302862+maestro125rus@users.noreply.github.com>
2023-06-12 03:51:55 +0300

    43.1 - Done

--- src/com/walking/lesson43_map/task1/Main.java
+++ src/com/walking/lesson43_map/task1/Main.java
@@ -1,6 +1,7 @@
 package com.walking.lesson43_map.task1;
 
-import java.util.Objects;
+import com.walking.lesson43_map.task1.excption.RegexValidationException;
+import java.util.*;
 
 /**
  * Реализуйте программу, выводящую в консоль количество использований каждого из уникальных слов
@@ -9,48 +10,39 @@ import java.util.Objects;
  * За основу предлагаю взять реализацию из задачи
  * <a href="https://github.com/KFalcon2022/practical-tasks/tree/master/src/com/walking/lesson30_regex/task3">...</a>.
  * <p>
- * "0"||"1"||"2"||"3"||"4"||"5"||"6"||"7"||"8"||"9"
  */
+
 public class Main {
+	public static final String WORDS_REGEX = "[a-z-\\s]+";
+
 	public static void main(String[] args) {
-		String str = " adad 123 kldfk";
-		System.out.println(myAtoi(str));
+		Scanner scanner = new Scanner(System.in);
 
-	}
+		System.out.print("Enter words: ");
+		String words = scanner.nextLine()
+				.trim()
+				.toLowerCase();
 
-	public static int myAtoi(String s) {
-		StringBuilder str = new StringBuilder();
-		for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) != ' ') {
-				str.append(s.charAt(i));
-			}
-		}
-		String[] strArr = new String[str.length()];
-		switch (strArr[0]){
-			case "-", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9":
+		scanner.close();
 
+		if (!words.matches(WORDS_REGEX)) {
+			throw new RegexValidationException(words, WORDS_REGEX);
 		}
 
+		String[] wordArray = words.split("\\s+");
+		Map <String, Integer>  map = new HashMap<>();
 
-		return 0;
-	}
-
-/*		if (!str.isEmpty()){
-			StringBuilder strResult = new StringBuilder();
-			switch (str.charAt(0)) {
-				case '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' -> {
-					for (int i = 0; i < str.length(); i++) {
-						switch (s.charAt(i)) {
-							case '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' -> strResult.append(s.charAt(i));
-						}
-					}
-				}
-				default -> {
-					return 0;
-				}
+		for (String word: wordArray) {
+			if (!map.containsKey(word)){
+				map.put(word, 1);
+			}else {
+				map.put(word, map.get(word) + 1);
 			}
-			return Integer.parseInt(strResult.toString());
-		}else {
-			return 0;
-		}*/
+		}
+
+		Set<Map.Entry<String, Integer>> set = map.entrySet();
+		for (Object obj : set) {
+			System.out.println(obj);
+		}
+	}
 }
