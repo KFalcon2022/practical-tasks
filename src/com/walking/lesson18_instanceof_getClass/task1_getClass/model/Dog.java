c3ad6b606f2841b789e4b70b59b571be91469d81
Brillionb <welkommen@yandex.ru>
2023-06-20 21:11:58 +0300

    Lesson 18

---
+++ src/com/walking/lesson18_instanceof_getClass/task1_getClass/model/Dog.java
@@ -0,0 +1,10 @@
+package com.walking.lesson18_instanceof_getClass.task1_getClass.model;
+
+public class Dog extends Animal {
+
+    public void woof() {
+        sound("woof");
+    }
+}
+
+
---
+++ src/com/walking/lesson18_instanceof_getClass/task1_instanceof/model/Animal.java
@@ -0,0 +1,8 @@
+package com.walking.lesson18_instanceof_getClass.task1_instanceof.model;
+
+public class Animal {
+
+    protected void sound(String animalSound) {
+        System.out.println(animalSound);
+    }
+}
