424c0c368e0994cbbff93b4542c02fa926a97aab
MaxxTi <ti.max1@yandex.ru>
2023-11-07 09:21:04 +0300

    задачa 2 урока 41

--- src/com/walking/lesson41_set/task2/Main.java
+++ src/com/walking/lesson41_set/task2/Main.java
@@ -1,5 +1,10 @@
 package com.walking.lesson41_set.task2;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 /**
  * Реализуйте задачу
  * <a href="https://github.com/KFalcon2022/practical-tasks/tree/master/src/com/walking/lesson26_string_types/task2">...</a>,
@@ -7,5 +12,18 @@ package com.walking.lesson41_set.task2;
  */
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Введи слова через пробел: ");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        scanner.close();
+
+        str.trim();
+        String[] strArr = str.split(" ");
+
+        Set uniqueWords = new HashSet(Arrays.asList(strArr));
+
+        System.out.println(uniqueWords.size());
+
+        System.out.println(uniqueWords);
     }
 }
