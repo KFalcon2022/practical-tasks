1ee4cec675e14e71fbbb989bb9ead013db1647f6
MaxxTi <ti.max1@yandex.ru>
2023-11-07 08:13:23 +0300

    исправил задачу урока 40

---
+++ src/com/walking/lesson41_set/task1/service/TaskService.java
@@ -0,0 +1,10 @@
+package com.walking.lesson41_set.task1.service;
+
+import com.walking.lesson41_set.task1.model.Task;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class TaskService {
+    Set<Task> tasks = new LinkedHashSet<>();
+}
