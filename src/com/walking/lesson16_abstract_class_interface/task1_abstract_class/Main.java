334cb542300695a2bc0157b3474e401fd48b8e9a
Kot <kot@mailforgit.qzaaa>
2023-11-16 12:06:07 +0300

    My version

--- src/com/walking/lesson16_abstract_class_interface/task1_abstract_class/Main.java
+++ src/com/walking/lesson16_abstract_class_interface/task1_abstract_class/Main.java
@@ -1,5 +1,11 @@
 package com.walking.lesson16_abstract_class_interface.task1_abstract_class;
 
+
+import com.walking.lesson16_abstract_class_interface.task1_abstract_class.model.Square;
+import com.walking.lesson16_abstract_class_interface.task1_abstract_class.model.Triangle;
+
+import java.util.Scanner;
+
 /**
  * Реализуйте задачу
  * <a href="https://github.com/KFalcon2022/practical-tasks/tree/master/src/com/walking/lesson14_polymorphism/task1">...</a>
@@ -7,5 +13,37 @@ package com.walking.lesson16_abstract_class_interface.task1_abstract_class;
  */
 public class Main {
     public static void main(String[] args) {
+        byte selectedFigure, selectedLength;
+
+        Scanner io = new Scanner(System.in);
+
+        System.out.println("What figure do you want? [1 - triangle, 2 - square]");
+        selectedFigure = io.nextByte();
+        System.out.println("Enter side's length [min - 3, max - 20]");
+        selectedLength = io.nextByte();
+        io.close();
+
+//        Checking user's validity
+        if (selectedFigure <1 || selectedFigure > 2 || selectedLength < 3 || selectedLength > 20){
+            System.out.println("Invalid entry");
+            return;
+        }
+        System.out.println("Entry is VALID. Drawing your figure...\n");
+
+        if (selectedFigure == 1){
+            Triangle a = new Triangle(selectedLength);
+            System.out.println(a.builtFigure());
+        }
+        else {
+            Square a = new Square(selectedLength);
+            System.out.println(a.builtFigure());
+        }
+
+
+
+
+
+
+
     }
 }
---
+++ src/com/walking/lesson16_abstract_class_interface/task1_abstract_class/model/Triangle.java
@@ -0,0 +1,37 @@
+package com.walking.lesson16_abstract_class_interface.task1_abstract_class.model;
+
+public class Triangle extends Figure{
+
+    public Triangle(int width){
+        super(width);
+    }
+
+    @Override
+    public String builtFigure() {
+        StringBuilder wholeFigure = new StringBuilder();
+        StringBuilder sideSpaces = new StringBuilder();
+        StringBuilder innerSpaces = new StringBuilder();
+        byte spaceCount = (byte) ((width-2) / 2);
+        byte innerSpaceCount = 0;
+
+        for (byte y = spaceCount; y >= 0; y--) {
+            for (byte x = 0; x <= y; x++) {
+                sideSpaces.append(FILLING_SYMBOL);
+            }
+
+            for (byte x = 0; x < innerSpaceCount; x++) {
+                if (y != 0) innerSpaces.append(FILLING_SYMBOL);
+                else innerSpaces.append(HORIZONTAL_SYMBOL);
+            }
+
+            wholeFigure.append(sideSpaces).append(TRIANGLE_LEFT_SYMBOL).append(innerSpaces).append(TRIANGLE_RIGHT_SYMBOL).append(sideSpaces).append("\n");
+            sideSpaces = new StringBuilder();
+            innerSpaces = new StringBuilder();
+            innerSpaceCount += 2;
+        }
+
+        return wholeFigure.toString();
+    }
+
+
+}
