986ec0586d9a840dff848f2e5b9745079ecccedd
N1ruingit config --global user.name N1ruin <n1ruin123@gmail.com>
2024-01-15 20:24:54 +0300

    Task3Lesson6

--- src/com/walking/lesson6_methods/Task3.java
+++ src/com/walking/lesson6_methods/Task3.java
@@ -2,71 +2,43 @@ package com.walking.lesson6_methods;
 
 import java.util.Scanner;
 
-/**
- * Написать программу, которая принимает длину и ширину прямоугольника (2 целых числа).
- * Нарисовать в консоли заданный прямоугольник, используя “-“ и “|”.
- * Углы прямоугольника обозначить символом “ “.
- * Каждая единица длины должна обозначаться одним символом “-“,
- * каждая единица ширины – символом “|“.
- * <p>
- * Произвести декомпозицию по своему усмотрению. Рекомендую скинуть на проверку. Контакт ниже.
- */
 public class Task3 {
+    /*
+    Написать программу, которая принимает длину и ширину прямоугольника (2 целых числа). Нарисовать в консоли заданный
+    прямоугольник, используя “-“ и “|”. Углы прямоугольника обозначить символом “ “. Каждая единица длины должна
+    обозначаться одним символом “-“, каждая единица ширины – символом “|“.
+    Произвести декомпозицию по своему усмотрению. Рекомендую скинуть на проверку. Контакт ниже.
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        int length = requireInt(scanner, "Enter rectangle's length: ");
-        int width = requireInt(scanner, "Enter rectangle's width: ");
-
+        System.out.println("Введите длину прямоугольника: ");
+        int length = scanner.nextInt();
+        System.out.println("Введите ширину прямоугольника: ");
+        int width = scanner.nextInt();
         scanner.close();
+        StringBuilder string = new StringBuilder();
 
-        String horizontalLine = createHorizontalLine(length);
-        String verticalLines = createVerticalLines(length, width);
-
-        printRectangle(horizontalLine, verticalLines);
-    }
-
-    static int requireInt(Scanner scanner, String requiringMessage) {
-        System.out.print(requiringMessage);
-
-        return scanner.nextInt();
-    }
-
-    static void printRectangle(String horizontalLine, String verticalLines) {
-        System.out.print(horizontalLine + verticalLines + horizontalLine);
-    }
-
-    static String createHorizontalLine(int length) {
-        String horizontalLine = " ";
-
-        for (int i = 0; i < length; i++) {
-            horizontalLine += "-";
+        drawFirstAndLastLines(length, string);
+        for (int i = 1; i <= width - 2; i++) {
+            drawIntermediateLines(length, string);
         }
-
-        horizontalLine += " \n";
-
-        return horizontalLine;
+        drawFirstAndLastLines(length, string);
+        System.out.println(string);
     }
 
-    static String createVerticalLines(int length, int width) {
-        String verticalLinesUnit = getVerticalLinesUnit(length);
-
-        String verticalLines = "";
-
-        for (int i = 0; i < width; i++) {
-            verticalLines += verticalLinesUnit;
+    public static void drawFirstAndLastLines(int length, StringBuilder string) {
+        string.append(" ");
+        for (int i = 0; i < length - 1; i++) {
+            string.append("-");
         }
-        return verticalLines;
+        string.append(" \n");
     }
 
-    static String getVerticalLinesUnit(int length) {
-        String verticalLinesUnit = "|";
-
-        for (int i = 0; i < length; i++) {
-            verticalLinesUnit += " ";
+    public static void drawIntermediateLines(int length, StringBuilder string) {
+        string.append("|");
+        for (int i = 0; i < length - 1; i++) {
+            string.append(" ");
         }
-
-        verticalLinesUnit += "|\n";
-        return verticalLinesUnit;
+        string.append("|\n");
     }
 }
