bba53d6225594a58c492ac1cfbf8c06fb808fed7
N1ruingit config --global user.name N1ruin <n1ruin123@gmail.com>
2024-03-09 21:48:07 +0300

    Commit7

---
+++ src/com/walking/lesson13_inheritance/task2/model/Cow.java
@@ -0,0 +1,11 @@
+package com.walking.lesson13_inheritance.task2.model;
+
+public class Cow extends Animal {
+
+    public Cow() {
+        super("Cow");
+    }
+    public void moo() {
+        sound();
+    }
+}
