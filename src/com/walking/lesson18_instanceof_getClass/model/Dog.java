c2c0322bf969e710c2306403a5ad3134ebec1cb5
N1ruingit config --global user.name N1ruin <n1ruin123@gmail.com>
2024-03-14 22:30:24 +0300

    Commit9

---
+++ src/com/walking/lesson18_instanceof_getClass/model/Dog.java
@@ -0,0 +1,7 @@
+package com.walking.lesson18_instanceof_getClass.model;
+
+public class Dog extends Animal {
+    public void woof(Animal animal) {
+        sound(animal);
+    }
+}
--- src/com/walking/lesson18_instanceof_getClass/task1_getClass/Main.java
+++ src/com/walking/lesson18_instanceof_getClass/task1_getClass/Main.java
@@ -1,5 +1,11 @@
 package com.walking.lesson18_instanceof_getClass.task1_getClass;
 
+
+import com.walking.lesson18_instanceof_getClass.model.Animal;
+import com.walking.lesson18_instanceof_getClass.model.Cat;
+import com.walking.lesson18_instanceof_getClass.model.Cow;
+import com.walking.lesson18_instanceof_getClass.model.Dog;
+
 /**
  * Реализовать задачу
  * <a href="https://github.com/KFalcon2022/practical-tasks/tree/master/src/com/walking/lesson13_inheritance/task2">...</a>
@@ -7,5 +13,35 @@ package com.walking.lesson18_instanceof_getClass.task1_getClass;
  */
 public class Main {
     public static void main(String[] args) {
+        Animal[] animals = new Animal[10];
+        fillAnimalsArray(animals);
+        animalSound(animals);
+    }
+
+    private static void fillAnimalsArray(Animal[] animals) {
+        for (int i = 0; i < animals.length; i++) {
+            if (i % 2 == 0) {
+                animals[i] = new Cat();
+            } else if (i % 3 == 0) {
+                animals[i] = new Cow();
+            } else {
+                animals[i] = new Dog();
+            }
+        }
+    }
+
+    private static void animalSound(Animal[] animals) {
+        for (Animal animal : animals) {
+            if (animal.getClass().equals(Cat.class)) {
+                Cat cat = (Cat) animal;
+                cat.meow(animal);
+            } else if (animal.getClass().equals(Dog.class)) {
+                Dog dog = (Dog) animal;
+                dog.woof(animal);
+            } else if (animal.getClass().equals(Cow.class)) {
+                Cow cow = (Cow) animal;
+                cow.moo(animal);
+            }
+        }
     }
 }
