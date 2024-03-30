8c20f51b9541b93ec0c0d905259b377d108ebccd
Anton <goptarevanton@gmail.com>
2024-03-30 22:01:59 +0300

    Lesson 18 task 1. Выполнено.

---
+++ src/com/walking/lesson18_instanceof_getClass/Task1/Cat.java
@@ -0,0 +1,13 @@
+package com.walking.lesson18_instanceof_getClass.Task1;
+
+public class Cat extends Animal {
+
+    public Cat() {
+        super("meow");
+    }
+
+    public void meow() {
+        sound();
+    }
+
+}
---
+++ src/com/walking/lesson18_instanceof_getClass/Task1/Dog.java
@@ -0,0 +1,11 @@
+package com.walking.lesson18_instanceof_getClass.Task1;
+
+public class Dog extends Animal {
+    public Dog() {
+        super("woof");
+    }
+
+    public void woof() {
+        sound();
+    }
+}
