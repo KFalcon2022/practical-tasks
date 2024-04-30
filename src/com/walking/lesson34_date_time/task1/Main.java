46adbd2d2751aa18ea0b411a5f0a56455348d5c7
Binary-Cat-01 <kotpivowar@gmail.com>
2024-04-30 18:06:16 +0300

    Урок 34 (задачи 1,2) - ревью не нужно.

--- src/com/walking/lesson34_date_time/task1/Main.java
+++ src/com/walking/lesson34_date_time/task1/Main.java
@@ -1,10 +1,26 @@
 package com.walking.lesson34_date_time.task1;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Реализуйте метод, возвращающий объект LocalDateTime на основании переданной строки.
  * Пример входящей строки: 20.12.2022 20:16:00.
  */
 public class Main {
+    public static final DateTimeFormatter DATE_TIME_PATTERN = DateTimeFormatter.ofPattern("dd.MM.yyy kk:mm:ss");
+
     public static void main(String[] args) {
+        String testLocalDateTime = "20.12.2022 20:16:00";
+
+        LocalDateTime result = parseLocalDateTime(testLocalDateTime);
+
+        System.out.println(result.format(DATE_TIME_PATTERN));
+    }
+
+    public static LocalDateTime parseLocalDateTime(String localDateTimeCandidate) {
+        DateTimeFormatter pattern = DateTimeFormatter.ofPattern("dd.MM.yyyy kk:mm:ss");
+
+        return LocalDateTime.parse(localDateTimeCandidate, pattern);
     }
 }
