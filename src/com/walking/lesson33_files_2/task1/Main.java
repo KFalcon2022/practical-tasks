1f6dd88e528fe167c81341e88aac803dafe4497d
Binary-Cat-01 <kotpivowar@gmail.com>
2024-05-08 01:23:42 +0300

    Урок 33 (задачи 1,2) - на ревью.

--- src/com/walking/lesson33_files_2/task1/Main.java
+++ src/com/walking/lesson33_files_2/task1/Main.java
@@ -1,5 +1,10 @@
 package com.walking.lesson33_files_2.task1;
 
+import com.walking.lesson33_files_2.task1.model.Car;
+import com.walking.lesson33_files_2.task1.model.Color;
+import com.walking.lesson33_files_2.task1.repository.CarRepository;
+import com.walking.lesson33_files_2.task1.service.CarService;
+
 /**
  * Реализуйте Задачу 1 из урока
  * <a href="https://telegra.ph/Rabota-s-fajlami-CHast-I-12-17">...</a>
@@ -11,5 +16,38 @@ package com.walking.lesson33_files_2.task1;
  */
 public class Main {
     public static void main(String[] args) {
+        CarRepository carRepository = new CarRepository();
+        CarService carService = new CarService(carRepository);
+
+        System.out.println("Машины в репозитории после инициализации сервиса:");
+        carService.displayCars();
+
+        //добавление
+        carService.add(new Car("A123BC", 2024, Color.RED, false));
+        carService.add(new Car("Z000ZZ", 2000, Color.YELLOW, true));
+        carService.add(new Car("F999FF", 1970, Color.BLACK, true));
+
+        System.out.println("Машины после добавления:");
+        carService.displayCars();
+
+        //удаление
+        carService.remove(new Car("A123BC", 2024, Color.RED, false));
+
+        System.out.println("Машины после удаления:");
+        carService.displayCars();
+
+        //изменение информации
+        Car sampleCar = new Car("Z000ZZ", 2000, Color.YELLOW, true);
+        Car foundCar = carService.find(sampleCar);
+
+        if (foundCar != null) {
+            foundCar.setColor(Color.INDIGO);
+            foundCar.setFine(false);
+
+            carService.update();
+        }
+
+        System.out.println("Машины после изменения:");
+        carService.displayCars();
     }
 }
