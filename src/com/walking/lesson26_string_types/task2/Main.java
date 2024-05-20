d88d6e874f37ccc1a759196b6f34b092e2675d80
egor.djun <xedshotg@gmail.com>
2024-05-20 11:57:05 +0700

    lesson26 task2

--- src/com/walking/lesson26_string_types/task2/Main.java
+++ src/com/walking/lesson26_string_types/task2/Main.java
@@ -1,5 +1,7 @@
 package com.walking.lesson26_string_types.task2;
 
+import java.util.Scanner;
+
 /**
  * Реализуйте программу, принимающую с клавиатуры строку,
  * содержащую слова, разделенные пробелом.
@@ -12,5 +14,23 @@ package com.walking.lesson26_string_types.task2;
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner("aa bb cc aa vv");
+
+        System.out.print("Enter a words: ");
+        String text = scanner.nextLine();
+
+        String[] words = text.strip().toLowerCase().split(" ");
+        int count = 0;
+
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 1 + i; j < words.length; j++) {
+                if(words[i].equals(words[j]) && !words[i].equals(" ")) {
+                    words[j] = " ";
+                    count++;
+                }
+            }
+        }
+        System.out.println(words.length - count * 2);
     }
 }
+
