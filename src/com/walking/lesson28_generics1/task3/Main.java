de1582f187db84e3bdd71993c726cb8d4bd1ca4c
egor.djun <xedshotg@gmail.com>
2024-05-21 10:14:12 +0700

    lesson28 task3

--- src/com/walking/lesson28_generics1/task3/Main.java
+++ src/com/walking/lesson28_generics1/task3/Main.java
@@ -1,5 +1,9 @@
 package com.walking.lesson28_generics1.task3;
 
+import com.walking.lesson28_generics1.task3.exeption.ValueNotFound;
+import com.walking.lesson28_generics1.task3.model.GenericArray;
+
+
 /**
  * Реализовать класс для работы с массивом.
  * Разработать метод, производящий поиск значения в массиве.
@@ -7,6 +11,10 @@ package com.walking.lesson28_generics1.task3;
  * Если найдено — возвращать его.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ValueNotFound{
+        Integer[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        GenericArray<Integer> integerGenericArray = new GenericArray<>(array);
+
+        System.out.println(integerGenericArray.getValue(22));
     }
 }
