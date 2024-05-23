2c3156a875773a9a8096a3fafb3884165730da09
egor.djun <xedshotg@gmail.com>
2024-05-23 10:47:38 +0700

    lesson30 task2

--- src/com/walking/lesson30_regex/task2/Main.java
+++ src/com/walking/lesson30_regex/task2/Main.java
@@ -1,5 +1,8 @@
 package com.walking.lesson30_regex.task2;
 
+import com.walking.lesson30_regex.task2.exception.NameNotValid;
+import com.walking.lesson30_regex.task2.model.FullName;
+
 /**
  * Реализуйте метод для работы с ФИО.
  * Входным параметром должна являться строка,
@@ -13,6 +16,55 @@ package com.walking.lesson30_regex.task2;
  * Если невалидна – бросьте из метода исключение, указывающее на ошибку валидации.
  */
 public class Main {
+    private final static String FULL_NAME_REGEX = "^[А-Я][а-я-]+ [А-Я][а-я]+ [А-Я][а-я]+$";
+    private final static String DOUBLE_SURNAME_REGEX = "^[А-Я][а-я]+-[А-Я][а-я]+";
+    private final static String NAME_REGEX = "^[А-Я][а-я]+";
+    private final static String PATRONYMIC_REGEX = "[А-Я][а-я]+$";
+
     public static void main(String[] args) {
+        String dude = "Зубенко Михаил Петрович";
+        FullName fullName = createFullName(dude);
+        System.out.println(fullName);
+    }
+
+    private static FullName createFullName(String dude) {
+        if (!dude.matches(FULL_NAME_REGEX)) {
+            throw new NameNotValid("Ошибка валидации ФИО.");
+        }
+
+        String[] fullName = dude.split(" ");
+
+        String surname = fullName[0];
+        validateSurname(surname);
+
+        String name = fullName[1];
+        validateName(name);
+
+        String patronymic = fullName[2];
+        validatePatronymic(patronymic);
+
+        return new FullName(surname, name, patronymic);
+    }
+
+    private static void validateSurname(String name) {
+        if (name.contains("-")) {
+            if (!name.matches(DOUBLE_SURNAME_REGEX)) {
+                throw new NameNotValid("Ошибка валидации в двойной фамилии.");
+            }
+        } else {
+            validateName(name);
+        }
+    }
+
+    private static void validateName(String name) {
+        if (!name.matches(NAME_REGEX)) {
+            throw new NameNotValid("Ошибка валидации в фамилии или имени.");
+        }
+    }
+
+    private static void validatePatronymic(String name) {
+        if (!name.matches(PATRONYMIC_REGEX)) {
+            throw new NameNotValid("Ошибка валидации в отчестве.");
+        }
     }
 }
---
+++ src/com/walking/lesson30_regex/task2/exception/NameNotValid.java
@@ -0,0 +1,7 @@
+package com.walking.lesson30_regex.task2.exception;
+
+public class NameNotValid extends RuntimeException {
+    public NameNotValid(String message) {
+        super(message);
+    }
+}
