10c8af7728ff4952942e76afc2e0b705876295fc
egor.djun <xedshotg@gmail.com>
2024-06-04 10:34:43 +0700

    lesson43 task1

--- src/com/walking/lesson43_map/task1/Main.java
+++ src/com/walking/lesson43_map/task1/Main.java
@@ -1,5 +1,9 @@
 package com.walking.lesson43_map.task1;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  * Реализуйте программу, выводящую в консоль количество использований каждого из уникальных слов
  * во введенной пользователем строке.
@@ -9,5 +13,26 @@ package com.walking.lesson43_map.task1;
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a words: ");
+        String[] s = scanner.nextLine()
+                .trim()
+                .toLowerCase()
+                .split(" ");
+        scanner.close();
+
+        Map<String, Integer> uniqueWords = new HashMap<>();
+        uniqueWords.put(s[0], 1);
+
+        for (int i = 1; i < s.length; i++) {
+            if (uniqueWords.containsKey(s[i])) {
+                uniqueWords.put(s[i], uniqueWords.get(s[i]) + 1);
+            } else {
+                uniqueWords.put(s[i], 1);
+            }
+        }
+
+        System.out.println(uniqueWords);
     }
 }
