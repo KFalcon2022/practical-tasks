5907cf60f96da707e73f96dd91bd9dc89f4ceb4e
egor.djun <xedshotg@gmail.com>
2024-06-10 13:07:26 +0700

    lesson49 task2

---
+++ src/com/walking/lesson49_optional/task2/model/Housing.java
@@ -0,0 +1,13 @@
+package com.walking.lesson49_optional.task2.model;
+
+public class Housing {
+    private final String address;
+
+    public Housing(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
