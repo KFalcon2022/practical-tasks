0d2863452df6140937a63d0744de4d9ebe4053f0
egor.djun <xedshotg@gmail.com>
2024-06-15 13:00:32 +0700

    lesson57

--- src/com/walking/lesson57_stream_collect_collector/model/Department.java
+++ src/com/walking/lesson57_stream_collect_collector/model/Department.java
@@ -1,9 +1,65 @@
 package com.walking.lesson57_stream_collect_collector.model;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Department {
     private String name;
     private int vacancyAmount;
     private List<Employee> employees;
+
+    public Department(String name, int vacancyAmount, List<Employee> employees) {
+        this.name = name;
+        this.vacancyAmount = vacancyAmount;
+        this.employees = employees;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVacancyAmount() {
+        return vacancyAmount;
+    }
+
+    public void setVacancyAmount(int vacancyAmount) {
+        this.vacancyAmount = vacancyAmount;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "name='" + name + '\'' +
+                ", vacancyAmount=" + vacancyAmount +
+                ", employees=" + employees +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+
+        return getVacancyAmount() == that.getVacancyAmount()
+                && Objects.equals(getName(), that.getName())
+                && Objects.equals(getEmployees(), that.getEmployees());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getVacancyAmount(), getEmployees());
+    }
 }
--- src/com/walking/lesson57_stream_collect_collector/model/Employee.java
+++ src/com/walking/lesson57_stream_collect_collector/model/Employee.java
@@ -1,8 +1,76 @@
 package com.walking.lesson57_stream_collect_collector.model;
 
+import java.util.Objects;
+
 public class Employee {
     private String name;
     private boolean isMale;
     private String position;
     private int age;
+
+    public Employee(String name, boolean isMale, String position, int age) {
+        this.name = name;
+        this.isMale = isMale;
+        this.position = position;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isMale() {
+        return isMale;
+    }
+
+    public void setMale(boolean male) {
+        isMale = male;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", isMale=" + isMale +
+                ", position='" + position + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+
+        return isMale() == employee.isMale()
+                && getAge() == employee.getAge()
+                && Objects.equals(getName(), employee.getName())
+                && Objects.equals(getPosition(), employee.getPosition());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), isMale(), getPosition(), getAge());
+    }
 }
