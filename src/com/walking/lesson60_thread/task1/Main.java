465a095fd382d359a42b397e3ef971bc78cc7fc2
egor.djun <xedshotg@gmail.com>
2024-06-15 13:03:53 +0700

    lesson60 task1

--- src/com/walking/lesson60_thread/task1/Main.java
+++ src/com/walking/lesson60_thread/task1/Main.java
@@ -1,10 +1,14 @@
 package com.walking.lesson60_thread.task1;
 
+import com.walking.lesson60_thread.task1.thread.MyThread;
+
 /**
  * Напишите программу, которая пишет в консоль текущее время каждый две секунды,
  * пока программа запущена.
  */
 public class Main {
     public static void main(String[] args) {
+        Thread myThread = new Thread(new MyThread(), "MyThread");
+        myThread.start();
     }
 }
