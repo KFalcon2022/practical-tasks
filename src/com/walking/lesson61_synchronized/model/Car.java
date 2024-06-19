5542e61cbc9d9d897db914d209fa683846b5df53
egor.djun <xedshotg@gmail.com>
2024-06-19 14:44:10 +0700

    lesson61

---
+++ src/com/walking/lesson61_synchronized/model/Car.java
@@ -0,0 +1,43 @@
+package com.walking.lesson61_synchronized.model;
+
+public class Car {
+    private final CarIdentifier carIdentifier;
+    private String color;
+    private boolean actualTechnicalInspection;
+
+    public Car(String number, int year, String color, boolean actualTechnicalInspection) {
+        this(new CarIdentifier(number, year), color, actualTechnicalInspection);
+    }
+
+    public Car(CarIdentifier carIdentifier, String color, boolean actualTechnicalInspection) {
+        this.carIdentifier = carIdentifier;
+        this.color = color;
+        this.actualTechnicalInspection = actualTechnicalInspection;
+    }
+
+    public CarIdentifier getCarIdentifier() {
+        return carIdentifier;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isActualTechnicalInspection() {
+        return actualTechnicalInspection;
+    }
+
+    public void setActualTechnicalInspection(boolean actualTechnicalInspection) {
+        this.actualTechnicalInspection = actualTechnicalInspection;
+    }
+
+    @Override
+    public String toString() {
+        return "%s; %d; %s; %s;\n".formatted(getCarIdentifier().getNumber(),
+                getCarIdentifier().getYear(), color, actualTechnicalInspection ? "actual" : "no actual");
+    }
+}
