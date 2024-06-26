1c384e173d693a6445c957491a30e2719791d418
Roman <droii@inbox.ru>
2024-06-26 09:36:41 +0800

    feat:Added tasks from lesson20

--- src/com/walking/lesson20_exceptions/task2/Main.java
+++ src/com/walking/lesson20_exceptions/task2/Main.java
@@ -1,5 +1,13 @@
 package com.walking.lesson20_exceptions.task2;
 
+import java.util.Scanner;
+
+import com.walking.lesson16_abstract_class_interface.task1_interface.model.CorrectFigure;
+import com.walking.lesson16_abstract_class_interface.task1_interface.model.Square;
+import com.walking.lesson16_abstract_class_interface.task1_interface.model.Triangle;
+import com.walking.lesson20_exceptions.task2.Exception.InputValidationException;
+import com.walking.lesson20_exceptions.task2.Exception.InputValidationException.Reason;
+
 /**
  * Реализуйте любой из вариантов Задачи 1 в уроке 16.
  * При некорректном вводе с клавиатуры выбрасывайте собственное исключение InputValidationException.
@@ -8,6 +16,37 @@ package com.walking.lesson20_exceptions.task2;
  * Предка исключения определите самостоятельно.
  */
 public class Main {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
+        CorrectFigure figure=null;
+        System.out.print("Insert figure: 1-triangle, 2-square :");
+        Scanner sc=new Scanner(System.in);
+        int figureType=sc.nextInt();
+        System.out.print("Insert length:");
+        int length=sc.nextInt();
+        sc.close();
+        if (validateInput(figureType, length)){
+            switch (figureType) {
+                case 1:
+                    figure=new Triangle(length);
+                    break;
+                case 2:
+                    figure=new Square(length);
+                break;
+            }
+        }
+        if (figure!=null)
+        System.out.print(figure.printFigure());
+    }
+
+    private static boolean validateInput(int type, int length){
+        if (type!=1&type!=2){
+            throw new InputValidationException(Reason.WrongType);
+        }
+        if (length<=1){
+            throw new InputValidationException(Reason.WrongLength);
+        }
+        return true;
     }
 }
+
+
