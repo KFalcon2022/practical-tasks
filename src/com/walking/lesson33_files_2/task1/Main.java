6946313f9495b4dc1b80259b37d8565bb6d428df
Roman <droii@inbox.ru>
2024-07-11 14:53:30 +0800

    feat:Added tasks from lesson 33

--- src/com/walking/lesson33_files_2/task1/Main.java
+++ src/com/walking/lesson33_files_2/task1/Main.java
@@ -1,5 +1,10 @@
 package com.walking.lesson33_files_2.task1;
 
+import java.io.*;
+
+import com.walking.lesson32_files_1.task1.model.Car;
+import com.walking.lesson32_files_1.task1.model.Specification;
+
 /**
  * Реализуйте Задачу 1 из урока
  * <a href="https://telegra.ph/Rabota-s-fajlami-CHast-I-12-17">...</a>
@@ -11,5 +16,69 @@ package com.walking.lesson33_files_2.task1;
  */
 public class Main {
     public static void main(String[] args) {
+        File file=new File("./resource/","Cars.txt");
+        Car[] cars=initCars();
+        System.out.println(saveCarsFw(cars,file));    
+        System.out.println(saveCarsBos(cars, file));
+        System.out.println(saveCarsBw(cars, file));
+    }
+    private static Car[] initCars(){
+        Specification HyundaiPorter=new Specification("Hyundai", "Porter");
+        Specification NissanPrimera=new Specification("Nissan", "Primera");
+        Specification VolkswagenGolf=new Specification("Volkswagen", "Golf");
+        Car car1=new Car("м785ун", "x7mxkn7fp6m004121", HyundaiPorter, "White");
+        Car car2=new Car("м786ун", "x7mxkn7fp6m004122", HyundaiPorter, "Black");
+        Car car3=new Car("м787ун", "x7mxkn7fp6m004123", HyundaiPorter, "Grey");
+        Car car4=new Car("у462ун", "sjnbaap10u0107597", NissanPrimera, "Blue");
+        Car car5=new Car("у463ун", "sjnbaap10u0107598", NissanPrimera, "Blue");
+        Car car6=new Car("у464ун", "sjnbaap10u0107599", NissanPrimera, "Green");
+        Car car7=new Car("м131вв", "wvwzzz1k26w024582", VolkswagenGolf, "Grey");
+        Car car8=new Car("м231вв", "wvwzzz1k26w024583", VolkswagenGolf, "Grey");
+        Car[] cars={car1,car2,car3,car4,car5,car6,car7,car8};
+        return cars;
+    }
+
+    private static boolean saveCarsFw(Car[] cars, File file){
+        StringBuilder stringToSave=new StringBuilder();
+        for (Car car : cars) {
+            stringToSave.append(car.getStringToSave()+"\n");
+        }
+        try (FileWriter fw=new FileWriter(file)) {
+            fw.write(stringToSave.toString()); 
+            fw.flush();
+            return true;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+
+    }
+
+    private static boolean saveCarsBos(Car[] cars, File file){
+        byte[] buffer;
+        try (BufferedOutputStream bos=new BufferedOutputStream(new FileOutputStream(file))) {
+            for (Car car : cars) {
+                buffer=(car.getStringToSave()+"\n").getBytes();
+                bos.write(buffer);
+            }
+            return true;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+
+    }
+
+    private static boolean saveCarsBw(Car[] cars, File file){
+        try (BufferedWriter bw=new BufferedWriter(new FileWriter(file))) {
+            for (Car car : cars) {
+                bw.write(car.getStringToSave()+"\n");
+            }
+            return true;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+
     }
 }
--- src/com/walking/lesson33_files_2/task2/Main.java
+++ src/com/walking/lesson33_files_2/task2/Main.java
@@ -1,5 +1,13 @@
 package com.walking.lesson33_files_2.task2;
 
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.walking.lesson32_files_1.task1.model.Car;
+import com.walking.lesson32_files_1.task1.model.Specification;
+
 /**
  * Реализуйте Задачу 2 из урока
  * <a href="https://telegra.ph/Rabota-s-fajlami-CHast-I-12-17">...</a>
@@ -11,5 +19,83 @@ package com.walking.lesson33_files_2.task2;
  */
 public class Main {
     public static void main(String[] args) {
+        File file=new File("./resource/","Cars.txt");
+        printCars(loadCarsFr(file));
+        printCars(loadCarsBis(file));
+        printCars(loadCarsBr(file));
+    }
+
+    private static Car[] deserialize(StringBuilder input){
+        String stringCars=new String(input);
+        Car[] cars=new Car[count(stringCars,"gosNumber:")];
+        Pattern gosNumberPattern=Pattern.compile("gosNumber:[\\wа-яА-Я]+");
+        Pattern engineVinPattern=Pattern.compile("engineVin:[\\wа-яА-Я]+");
+        Pattern modelPattern=Pattern.compile("model:[\\wа-яА-Я]+");
+        Pattern brandPattern=Pattern.compile("brand:[\\wа-яА-Я]+");
+        Pattern colorPattern=Pattern.compile("color:[\\wа-яА-Я]+");
+        Matcher gosNumberMatcher=gosNumberPattern.matcher(stringCars);
+        Matcher engineVinMatcher=engineVinPattern.matcher(stringCars);
+        Matcher modelMatcher=modelPattern.matcher(stringCars);
+        Matcher brandMatcher=brandPattern.matcher(stringCars);
+        Matcher colorMatcher=colorPattern.matcher(stringCars);
+        for (int i = 0; i < cars.length; i++) {
+            gosNumberMatcher.find();
+            engineVinMatcher.find();
+            modelMatcher.find();
+            brandMatcher.find();
+            colorMatcher.find();
+            cars[i]=new Car(gosNumberMatcher.group().substring(10),
+             engineVinMatcher.group().substring(10),
+             new Specification(brandMatcher.group().substring(6), modelMatcher.group().substring(6)),
+              colorMatcher.group().substring(6));
+        }
+        return cars;
+    }
+
+    private static Car[] loadCarsBr(File file) {
+        StringBuilder temp=new StringBuilder();
+        try (BufferedReader br=new BufferedReader(new FileReader(file))) {
+            int c;
+            while ((c=br.read())!=-1) {
+                temp.append((char)c);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return deserialize(temp);
+    }
+
+    private static Car[] loadCarsBis(File file) {
+        StringBuilder temp=new StringBuilder();
+        try (BufferedInputStream bis=new BufferedInputStream(new FileInputStream(file))) {
+            temp.append(new String(bis.readAllBytes(), Charset.forName("UTF-8")));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return deserialize(temp);
+    }
+
+    private static Car[] loadCarsFr(File file) {
+        StringBuilder temp=new StringBuilder();
+        try (FileReader fr=new FileReader(file)) {
+            int c;
+            while ((c=fr.read())!=-1) {
+                temp.append((char)c);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return deserialize(temp);
+    }
+
+    private static void printCars(Car[] cars){
+        for (Car car : cars) {
+            System.out.println(car.toString());
+        }
+        System.out.println("\n");
+    }
+
+    public static int count(String str, String target) {
+        return (str.length() - str.replace(target, "").length()) / target.length();
     }
 }
