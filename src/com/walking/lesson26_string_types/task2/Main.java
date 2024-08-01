fbbeeb89af880d8e901e4e5224b39e9624e5154b
Arthur Whelan <dev.java.contact@gmail.com>
2024-08-01 17:49:51 +0300

    feat: сompleted lesson 26 task 2

--- src/com/walking/lesson26_string_types/task2/Main.java
+++ src/com/walking/lesson26_string_types/task2/Main.java
@@ -1,5 +1,7 @@
 package com.walking.lesson26_string_types.task2;
 
+import java.util.Scanner;
+
 /**
  * Реализуйте программу, принимающую с клавиатуры строку,
  * содержащую слова, разделенные пробелом.
@@ -12,5 +14,37 @@ package com.walking.lesson26_string_types.task2;
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your words: ");
+        String[] words = scanner.nextLine()
+                .trim()
+                .toLowerCase()
+                .split(" ");
+
+        scanner.close();
+
+        int uniqueWordsCounter = findUniqueWords(words);
+        System.out.println(uniqueWordsCounter);
+    }
+
+    private static int findUniqueWords(String[] words) {
+        int counter = 0;
+
+        for (int i = 0; i < words.length; i++) {
+            boolean isUnique = true;
+
+            for (int j = i + 1; j < words.length; j++) {
+                if (words[i].equals(words[j])) {
+                    isUnique = false;
+                    counter--;
+                }
+            }
+            if (isUnique) {
+                counter++;
+            }
+        }
+        return counter;
     }
 }
+
