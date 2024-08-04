224957c2bc024bd79b7d9e07da467b197d24033d
Binary-Cat-01 <kotpivowar@gmail.com>
2024-08-04 19:51:15 +0300

    Add solution of lesson 51 (iterator spliterator)

--- src/com/walking/lesson51_iterator_spliterator/Main.java
+++ src/com/walking/lesson51_iterator_spliterator/Main.java
@@ -1,5 +1,13 @@
 package com.walking.lesson51_iterator_spliterator;
 
+import java.util.Scanner;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import static java.util.Spliterator.*;
+
 /**
  * Реализовать Stream на основе System.in.
  * Операции в самом Stream’е можете описать на свой вкус.
@@ -8,5 +16,27 @@ package com.walking.lesson51_iterator_spliterator;
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Spliterator<String> spliterator = Spliterators.spliteratorUnknownSize(scanner, ORDERED & IMMUTABLE & NONNULL);
+
+        Stream<String> stringStream = StreamSupport.stream(spliterator, false);
+
+        System.out.println("Введите символ (для завершения введите x):");
+
+        stringStream.peek(s -> System.out.println("Количество сиволов - " + s.length()))
+                    .filter("x"::equals)
+                    .findFirst()
+                    .ifPresentOrElse(String::length, scanner::close);
+
+        System.out.println("Введите 3 числа:");
+
+        int sum = scanner.tokens()
+                         .filter(s -> s.matches("^\\d+$"))
+                         .limit(3)
+                         .mapToInt(Integer::parseInt)
+                         .sum();
+
+        System.out.printf("Сумма введенных чисел = %s", sum);
     }
 }
