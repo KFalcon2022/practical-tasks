30ae0d031a558c5b00893b1856d32cbfea45e398
Binary-Cat-01 <kotpivowar@gmail.com>
2024-08-05 16:21:46 +0300

    Add solution of lesson 52 (stream creation) task2

--- src/com/walking/lesson52_stream_creation/task2_limit/Main.java
+++ src/com/walking/lesson52_stream_creation/task2_limit/Main.java
@@ -1,10 +1,23 @@
 package com.walking.lesson52_stream_creation.task2_limit;
 
+import java.time.LocalDate;
+import java.util.stream.Stream;
+
 /**
  * Реализуйте программу, выводящую в консоль все даты текущего месяца.
  * Используйте Stream.limit()
  */
 public class Main {
     public static void main(String[] args) {
+        LocalDate localDate = LocalDate.now();
+
+        Stream.iterate(localDate.withDayOfMonth(1), d -> d.plusDays(1))
+              .limit(localDate.lengthOfMonth())
+              .forEach(System.out::println);
+
+        Stream.iterate(1, i -> ++i)
+              .map(localDate::withDayOfMonth)
+              .limit(localDate.lengthOfMonth())
+              .forEach(System.out::println);
     }
 }
