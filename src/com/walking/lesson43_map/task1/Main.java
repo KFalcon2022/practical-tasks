eaf0002e45726e3ed8f5188f4620c3df8eccccbf
Roman <droii@inbox.ru>
2024-08-21 15:44:04 +0800

    Add:tasks from lesson 43

--- src/com/walking/lesson43_map/task1/Main.java
+++ src/com/walking/lesson43_map/task1/Main.java
@@ -1,5 +1,13 @@
 package com.walking.lesson43_map.task1;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.walking.lesson28_generics1.task4.model.Stack;
+
 /**
  * Реализуйте программу, выводящую в консоль количество использований каждого из уникальных слов
  * во введенной пользователем строке.
@@ -9,5 +17,29 @@ package com.walking.lesson43_map.task1;
  */
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Insert some words:");
+        Scanner sc=new Scanner(System.in);
+        String input=sc.nextLine();
+        sc.close();
+        LinkedHashMap<String,Integer> map=getWordUseCount(input);
+        for (Map.Entry<String,Integer> entry : map.entrySet()) {
+            System.out.println("Word: "+entry.getKey()+" used "+entry.getValue()+" times");
+        }
+
+    }
+    private static LinkedHashMap<String,Integer> getWordUseCount(String s){
+        Pattern pattern=Pattern.compile("[а-яА-ЯA-Za-z-]+");
+        Matcher matcher=pattern.matcher(s);
+        LinkedHashMap<String,Integer> map=new LinkedHashMap<>();
+        while (matcher.find()) {
+            if (!matcher.group().matches("^.*-{2,}.*$")){
+                if (map.containsKey(matcher.group())){
+                    map.put(matcher.group(),map.get(matcher.group())+1);
+                } else{
+                    map.put(matcher.group(),1);
+                }
+            }
+        }
+        return map;
     }
 }
--- src/com/walking/lesson43_map/task2/Main.java
+++ src/com/walking/lesson43_map/task2/Main.java
@@ -1,5 +1,24 @@
 package com.walking.lesson43_map.task2;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
+import com.walking.lesson19_object_methods.model.Car;
+
+
+
+/**
+ * Реализуйте класс «Машина». Поля допустимо выбрать на свое усмотрение, но необходимо,
+ * чтобы по ним можно было однозначно идентифицировать каждую машину.
+ * Скажем, в рамках базы ГАИ.
+ * <p>
+ * Создайте массив машин. Реализуйте максимально эффективную проверку на вхождение машины в ваш массив.
+ * Данные для проверки необходимо запрашивать с клавиатуры.
+ * <p>
+ * Если машина найдена — выведите ее строковое представление в консоль.
+ * <p>
+ * Опциональное усложнение: номер машины может быть не уникальным.
+ */
 /**
  * Реализуйте задачу
  * <a href="https://github.com/KFalcon2022/practical-tasks/tree/master/src/com/walking/lesson19_object_methods">...</a>,
@@ -8,5 +27,33 @@ package com.walking.lesson43_map.task2;
  */
 public class Main {
     public static void main(String[] args) {
+        HashMap<Integer,Car> cars=new HashMap<>();
+        Car temp=new Car("у462ун", "sjnbaap10u0107597",
+        "Nissan" , "Primera", "Blue");
+        cars.putIfAbsent(temp.hashCode(), temp);
+        temp=new Car("р758ае", "wdb1400321a269616",
+        "Mercedes-benz", "9320", "Blue");
+        cars.putIfAbsent(temp.hashCode(), temp);
+        temp=new Car("м785ун","x7mxkn7fp6m004128",
+        "Hyundai", "Porter", "White");
+        cars.putIfAbsent(temp.hashCode(), temp);
+        temp=new Car("м131вв", "wvwzzz1k26w024582",
+        "Volkswagen", "Golf", "Grey");
+        cars.putIfAbsent(temp.hashCode(), temp);
+        String inputGosNumber="";
+        String inputEngineVin="";
+        System.out.println("Insert gosNumber:");
+        Scanner sc=new Scanner(System.in);
+        inputGosNumber=sc.nextLine();
+        System.out.println("Insert engineVin");
+        inputEngineVin=sc.nextLine();
+        sc.close();
+        temp=new Car(inputGosNumber, inputEngineVin);
+        if (cars.containsKey(temp.hashCode())&&cars.get(temp.hashCode()).equals(temp)){
+            System.out.println(cars.get(temp.hashCode()).toString());
+        } else {
+            System.out.println("Not found");
+        }
+
     }
 }
