d6417a6b13853fd19552debe3a52692526c5351f
Rita <margarita.mamatkazina@gmail.com>
2025-03-17 17:08:54 +0300

    правки lesson 16 task2

--- src/com/walking/lesson16_abstract_class_interface/task2/printer/Hello.java
+++ src/com/walking/lesson16_abstract_class_interface/task2/printer/Hello.java
@@ -3,10 +3,6 @@ package com.walking.lesson16_abstract_class_interface.task2.printer;
 public class Hello implements Printer {
     final static String RESPOND = "Hello";
 
-    public Hello() {
-
-    }
-
     public String print() {
        return RESPOND;
     }
--- src/com/walking/lesson16_abstract_class_interface/task2/printer/How.java
+++ src/com/walking/lesson16_abstract_class_interface/task2/printer/How.java
@@ -3,10 +3,6 @@ package com.walking.lesson16_abstract_class_interface.task2.printer;
 public class How implements Printer {
     final static String RESPOND = "How are you doing";
 
-    public How() {
-
-    }
-
     public String print() {
         return RESPOND;
     }
