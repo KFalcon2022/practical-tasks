6b72e7e10d80d6bfb924d41e2859576658da5469
Rita <margarita.mamatkazina@gmail.com>
2025-03-18 21:00:59 +0300

    правки lesson 17 task2

--- src/com/walking/lesson17_enum/task2/animals/Animal.java
+++ src/com/walking/lesson17_enum/task2/animals/Animal.java
@@ -1,4 +1,15 @@
 package com.walking.lesson17_enum.task2.animals;
 
 public enum Animal {
+    CAT("meow"), Dog("woof"), Cow("moo");
+
+    private final String sound;
+
+    Animal(String sound) {
+        this.sound = sound;
+    }
+
+    public String getSound() {
+        return sound;
+    }
 }
