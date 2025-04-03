ba035de9f8e85a1a6b37f5b1ae0392a76dce9bb9
Rita <margarita.mamatkazina@gmail.com>
2025-04-03 13:49:07 +0300

    lesson 21

--- src/com/walking/lesson21_immutable_object/autos/Car.java
+++ src/com/walking/lesson21_immutable_object/autos/Car.java
@@ -2,7 +2,7 @@ package com.walking.lesson21_immutable_object.autos;
 
 public class Car {
     private final CarIdentifier identifier;
-    private final String owner;
+    private String owner;
 
     public Car(String brand, String owner, String number) {
         this.identifier = new CarIdentifier(brand, number);
@@ -16,6 +16,14 @@ public class Car {
 
     public CarIdentifier getIdentifier() {
         return identifier;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
     }
 
 }
