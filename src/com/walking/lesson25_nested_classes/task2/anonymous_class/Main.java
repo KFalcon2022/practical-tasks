c975f8353dba8e932ff92d2a197e28205d9ad2b8
Rita <margarita.mamatkazina@gmail.com>
2025-04-15 11:47:39 +0300

    lesson 25, task 2, анонимные классы

--- src/com/walking/lesson25_nested_classes/task2/anonymous_class/Main.java
+++ src/com/walking/lesson25_nested_classes/task2/anonymous_class/Main.java
@@ -1,5 +1,7 @@
 package com.walking.lesson25_nested_classes.task2.anonymous_class;
 
+import com.walking.lesson25_nested_classes.task2.anonymous_class.animal.Animal;
+
 /**
  * Реализуйте задачу
  * <a href="https://github.com/KFalcon2022/practical-tasks/tree/master/src/com/walking/lesson14_polymorphism/task2">...</a>
@@ -7,5 +9,34 @@ package com.walking.lesson25_nested_classes.task2.anonymous_class;
  */
 public class Main {
     public static void main(String[] args) {
+        Animal[] animals = new Animal[]{new Animal() {
+            @Override
+            public void sound() {
+                System.out.println("meow");
+            }
+        }, new Animal() {
+            @Override
+            public void sound() {
+                System.out.println("woof");
+            }
+        }, new Animal() {
+            @Override
+            public void sound() {
+                System.out.println("moo");
+            }
+        }, new Animal() {
+            @Override
+            public void sound() {
+                System.out.println("meow");
+            }
+        }};
+
+        soundAll(animals);
+    }
+
+    private static void soundAll(Animal[] animals) {
+        for (Animal animal : animals) {
+            animal.sound();
+        }
     }
 }
