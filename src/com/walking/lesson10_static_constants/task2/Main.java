60068d1664e1723de0ea725c0dffc81ab205f2dc
AlexeyNiyazov <psychotrance92@yandex.ru>
2025-04-17 17:25:29 +1200

    lesson 10 task 2

--- src/com/walking/lesson10_static_constants/task2/Main.java
+++ src/com/walking/lesson10_static_constants/task2/Main.java
@@ -1,13 +1,36 @@
 package com.walking.lesson10_static_constants.task2;
 
 /**
- * Для задачи
- * <a href="https://github.com/KFalcon2022/practical-tasks/tree/master/src/lesson8_classes_objects">...</a>
- * реализуйте неизменность поля названия у класса Counter.
- * Ведь очень странно, если мы можем менять название счетчика по ходу выполнения программы,
- * не так ли?
+ * Реализовать класс-счетчик.
+ * Полями класса должны выступать название счетчика и, непосредственно, целочисленный счетчик.
+ * <p>
+ * Реализовать для созданного класса конструктор с двумя параметрами,
+ * с одним (значение счетчика в таком случае инициализировать как 0),
+ * методы увеличения и уменьшения счетчиков на 1 и на заданное пользователем целом значение.
+ * Методы должны возвращать актуальное значение счетчика.
+ * <p>
+ * Используя созданный класс,
+ * посчитать количество четных и количество нечетных чисел в ряду от 1 до 100.
+ * Конечные показатели счетчиков вывести в консоль.
  */
 public class Main {
     public static void main(String[] args) {
+        Counter evenCounter = new Counter("Четные числа");
+        Counter oddCounter = new Counter("Нечетные числа");
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                evenCounter.increment();
+            } else {
+                oddCounter.increment();
+            }
+        }
+
+        printCounterValue(evenCounter);
+        printCounterValue(oddCounter);
+    }
+
+    static void printCounterValue(Counter counter) {
+        System.out.printf("%s: %d\n", counter.NAME, counter.counter);
     }
 }
