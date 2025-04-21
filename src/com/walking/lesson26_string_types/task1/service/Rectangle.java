0a713f05ac23aaa715494d1259c321a74c4089b4
Rita <margarita.mamatkazina@gmail.com>
2025-04-21 14:04:51 +0300

    урок 26, задание 1

---
+++ src/com/walking/lesson26_string_types/task1/service/Rectangle.java
@@ -0,0 +1,44 @@
+package com.walking.lesson26_string_types.task1.service;
+
+public class Rectangle {
+    private final int length;
+    private final int width;
+    private final String SPACE = " ";
+    private final String LENGTH_SYMBOL = "-";
+    private final String WIDTH_SYMBOL = "|";
+
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    /**
+     * как я поняла, StringBuilder быстрее работает,
+     * а плюсы StringBuffer'а только в условиях многопоточности раскрываются, что, вроде, не про эту задачу.
+     * поэтому использовала StringBuilder
+     * P.S. может, перемудрила с классами, но мне захотелось попробовать поприменять из прошлых уроков штуки:)
+     */
+
+    public StringBuilder getRectangle() {
+        StringBuilder rectangle = getLine(SPACE, LENGTH_SYMBOL);
+        for (int i = 0; i < width - 2; i++) {
+            rectangle.append(getLine(WIDTH_SYMBOL, SPACE));
+        }
+        return rectangle.append(getLine(SPACE, LENGTH_SYMBOL));
+    }
+
+    private StringBuilder getLine(String side, String center) {
+        StringBuilder horizontal = new StringBuilder(side);
+        horizontal.append(center.repeat(length - 2));
+
+        return horizontal.append(side).append("\n");
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+}
