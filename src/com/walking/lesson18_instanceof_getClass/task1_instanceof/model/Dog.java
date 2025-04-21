40816a9f6222a3e822878293a3d5e677c26caee9
AlexeyNiyazov <psychotrance92@yandex.ru>
2025-04-21 20:45:39 +1200

    Lesson 18 task 1 - 2

---
+++ src/com/walking/lesson18_instanceof_getClass/task1_instanceof/model/Dog.java
@@ -0,0 +1,13 @@
+package com.walking.lesson18_instanceof_getClass.task1_instanceof.model;
+
+public class Dog extends Animal {
+    //    Передаем параметры в super внутри конструктора, а не принимая их в параметры Cat(),
+//    чтобы избежать ошибочного поведения при использовании этого класса
+    public Dog() {
+        super("woof");
+    }
+
+    public void woof() {
+        sound();
+    }
+}
