7202d846c185fb29034580b41601e97639e7cca5
Yahor <egor.2000.xaxaxa@gmail.com>
2025-05-10 16:24:15 +0400

    Lesson 171

---
+++ src/com/walking/lesson171_annotations/task1/Main.java
@@ -0,0 +1,10 @@
+package com.walking.lesson171_annotations.task1;
+
+/**
+ * Реализуйте метод, который принимает на вход список объектов <code>Class</code>
+ * и записывает в консоль имена помеченных аннотацией <code>@Deprecated</code>.
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
