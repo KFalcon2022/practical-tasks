c93f510c1a3e0d0416dea6264be553ea62799c71
Rita <margarita.mamatkazina@gmail.com>
2025-05-13 12:55:46 +0300

    lesson 28, task 2

--- src/com/walking/lesson28_generics1/task2/Main.java
+++ src/com/walking/lesson28_generics1/task2/Main.java
@@ -1,10 +1,24 @@
 package com.walking.lesson28_generics1.task2;
 
+import com.walking.lesson28_generics1.task2.service.GenericObject;
+
 /**
  * Создать класс-обертку над объектом любого типа.
  * Предусмотреть boolean-метод, проверяющий значение объекта на null.
  */
 public class Main {
     public static void main(String[] args) {
+        GenericObject<String> stringObj = new GenericObject<>("Hello");
+        GenericObject<Number> numberObj = new GenericObject<>(null);
+        print(stringObj);
+        print(numberObj);
+    }
+
+    public static void print(GenericObject object) {
+        if (object.isNull()) {
+            throw new RuntimeException("Object is null");
+        }
+
+        System.out.println(object.getObj());
     }
 }
