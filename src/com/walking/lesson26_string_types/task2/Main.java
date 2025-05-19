349fc6bd48cce2e4b4d342de1279e31a2c0f129d
AlexeyNiyazov <psychotrance92@yandex.ru>
2025-05-19 19:37:31 +1200

    Lesson 26 task2

--- src/com/walking/lesson26_string_types/task2/Main.java
+++ src/com/walking/lesson26_string_types/task2/Main.java
@@ -1,5 +1,7 @@
 package com.walking.lesson26_string_types.task2;
 
+import java.util.Scanner;
+
 /**
  * Реализуйте программу, принимающую с клавиатуры строку,
  * содержащую слова, разделенные пробелом.
@@ -18,5 +20,41 @@ package com.walking.lesson26_string_types.task2;
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a words: ");
+        StringBuffer strings = new StringBuffer(scanner.nextLine().trim());
+
+        scanner.close();
+
+        String[] array = strings.toString().split(" ");
+        System.out.println("ИЛИ ЖЕ ПРОЩЕ СОЗДАТЬ МАССИВ STRINGBUILDER И ЗНАЧЕНИЕ КАЖДОГО МАССИВА ПРИВОДИТЬ К СТРОКЕ??" +
+                "ОТВЕТЬ ПЖ В КОМЕНТАРИЯХ К ПР)");
+
+        int wordsCounter = 0;
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (i == 0) {
+                wordsCounter++;
+                continue;
+            }
+            if (array[i].isBlank()) {
+                continue;
+            }
+
+            for (int j = 0; j < i; j++) {
+
+                if (array[i].compareToIgnoreCase(array[j]) == 0) {
+                    break;
+                } else if (j < i - 1) {
+                    continue;
+                } else {
+                    wordsCounter++;
+                }
+            }
+        }
+
+        System.out.println(wordsCounter);
     }
 }
