d938ac3425e4d16756680a08ee1b793021a9694a
AlexeyNiyazov <psychotrance92@yandex.ru>
2025-05-22 16:57:18 +1200

    Lesson 28 task2&task3

---
+++ src/com/walking/lesson28_generics1/task3/service/ValueSearcher.java
@@ -0,0 +1,22 @@
+package com.walking.lesson28_generics1.task3.service;
+
+import com.walking.lesson28_generics1.task3.exception.ValueNotFoundException;
+
+public class ValueSearcher<T> {
+    private final T[] array;
+
+    public ValueSearcher(T[] array) {
+        this.array = array;
+    }
+
+    public T search(T requiredValue) {
+
+        for (T arrayValue : array) {
+            if (arrayValue.equals(requiredValue)) {
+                return arrayValue;
+            }
+        }
+
+        throw new ValueNotFoundException("Required Value is not found");
+    }
+}
