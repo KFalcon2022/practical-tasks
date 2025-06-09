b589bae937253ecaa114404e5f0b65445102dfbc
AlexeyNiyazov <psychotrance92@yandex.ru>
2025-06-10 06:34:49 +1200

    PR changes

--- src/com/walking/lesson28_generics1/task4/exception/StackNotFountException.java
+++
@@ -1,7 +0,0 @@
-package com.walking.lesson28_generics1.task4.exception;
-
-public class StackNotFountException extends RuntimeException {
-    public StackNotFountException(String message) {
-        super(message);
-    }
-}
