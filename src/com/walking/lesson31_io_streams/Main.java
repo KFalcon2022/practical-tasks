cb2ee42f948e1ef64ee1c4696b7d71e7aaa12ae9
Rita <margarita.mamatkazina@gmail.com>
2025-06-11 15:46:02 +0300

    lesson 31

--- src/com/walking/lesson31_io_streams/Main.java
+++ src/com/walking/lesson31_io_streams/Main.java
@@ -3,18 +3,19 @@ package com.walking.lesson31_io_streams;
 import com.walking.lesson31_io_streams.exeption.WrongTypeExeption;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 /**
  * Чтение данных с помощью System.in и приведение к примитивным типам или строкам
  */
 public class Main {
     public static void main(String[] args) throws IOException {
+        System.out.println(readString());
+        System.out.println(readBoolean());
         System.out.println(readChar());
         System.out.println(readByte());
         System.out.println(readInt());
-        System.out.println(readString());
-        System.out.println(readBoolean());
-
     }
 
     public static byte readByte() throws IOException {
@@ -49,10 +50,11 @@ public class Main {
 
     public static String readString() throws IOException {
         StringBuilder stringBuilder = new StringBuilder();
+        Reader reader = new InputStreamReader(System.in);
         boolean isFinished = false;
 
         while (!isFinished) {
-            char ch = (char) System.in.read();
+            char ch = (char) reader.read();
 
             if (ch == '\n') {
                 isFinished = true;
