1d546f22b5b7c3f7f63e576924ae083fc25179f9
Rita <margarita.mamatkazina@gmail.com>
2025-07-03 19:53:49 +0300

    lesson 33, task 2

--- src/com/walking/lesson33_files_2/task2/var2/Main.java
+++ src/com/walking/lesson33_files_2/task2/var2/Main.java
@@ -1,27 +1,17 @@
-package com.walking.lesson33_files_2.task2;
+package com.walking.lesson33_files_2.task2.var2;
 
 import com.walking.lesson19_object_methods.car.Car;
 
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
-/**
- * Реализуйте Задачу 2 из урока
- * <a href="https://telegra.ph/Rabota-s-fajlami-CHast-I-12-17">...</a>
- * с помощью:
- * <p>
- * Вариант 1: FileReader;
- * Вариант 2: BufferedInputStream;
- * Вариант 3: BufferedReader.
- */
 public class Main {
     public static final String FILE_LOCATION = "./resources/carCatalog.txt";
 
     public static void main(String[] args) throws IOException {
-        String text = readFile2();
+        String text = readFile();
         Car[] cars = getCars(getCarInformation(text));
         Scanner in = new Scanner(System.in);
         System.out.print("Номер машины в списке: ");
@@ -32,18 +22,7 @@ public class Main {
         System.out.println(cars[i - 1].getNumber());
     }
 
-    public static String readFile1() throws IOException {
-        StringBuilder cars = new StringBuilder();
-        try (FileReader reader = new FileReader(FILE_LOCATION)) {
-            int i;
-            while ((i = reader.read()) != -1) {
-                cars.append((char) i);
-            }
-        }
-        return cars.toString().trim();
-    }
-
-    public static String readFile2() throws IOException {
+    public static String readFile() throws IOException {
         StringBuilder cars = new StringBuilder();
         try (FileInputStream fis = new FileInputStream(FILE_LOCATION);
              BufferedInputStream bis = new BufferedInputStream(fis)) {
