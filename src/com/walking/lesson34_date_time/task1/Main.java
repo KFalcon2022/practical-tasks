c738c0b24232d3edaf1649061b900f2abc013638
Rita <margarita.mamatkazina@gmail.com>
2025-07-11 15:19:43 +0300

    lesson 34, task 1

--- src/com/walking/lesson34_date_time/task1/Main.java
+++ src/com/walking/lesson34_date_time/task1/Main.java
@@ -1,10 +1,30 @@
 package com.walking.lesson34_date_time.task1;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 /**
  * Реализуйте метод, возвращающий объект LocalDateTime на основании переданной строки.
  * Пример входящей строки: 20.12.2022 20:16:00.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.y HH:mm:ss");
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.print("Введите дату: ");
+        Scanner in = new Scanner(System.in);
+        String date = in.nextLine();
+
+        LocalDateTime dateTime = getLocalDateTime(date);
+        System.out.println(dateTime);
+    }
+
+    public static LocalDateTime getLocalDateTime(String dateTime) {
+        try {
+            return LocalDateTime.parse(dateTime, formatter);
+        } catch (Exception e) {
+            throw new RuntimeException("Дата указана некорректно.");
+        }
     }
 }
