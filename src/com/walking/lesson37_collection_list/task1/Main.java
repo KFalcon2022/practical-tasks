afd25bace0d1502bd4d9f0ddbdabe71d5bfef360
AlexeyNiyazov <psychotrance92@yandex.ru>
2025-07-20 15:35:29 +1200

    Lesson 34 task2-3 Lesson 37 task1
    Signed-off-by: AlexeyNiyazov <psychotrance92@yandex.ru>

--- src/com/walking/lesson37_collection_list/task1/Main.java
+++ src/com/walking/lesson37_collection_list/task1/Main.java
@@ -1,5 +1,10 @@
 package com.walking.lesson37_collection_list.task1;
 
+import com.walking.lesson37_collection_list.task1.model.Counter;
+import com.walking.lesson37_collection_list.task1.service.CounterService;
+
+import java.util.List;
+
 /**
  * Реализуйте задачу
  * <a href="https://github.com/KFalcon2022/CounterAggregation">...</a>, используя ArrayList.
@@ -10,6 +15,23 @@ package com.walking.lesson37_collection_list.task1;
  * Изменилось ли что-то в кодовой базе при использовании списка другой реализации?
  */
 public class Main {
+    private static final String HOT_WATER_COUNTER_NAME = "Hot water";
+    private static final String COLD_WATER_COUNTER_NAME = "Cold water";
+    private static final String ELECTRICITY_COUNTER_NAME = "Electricity";
+
+    private static final String WATER_UNIT = "m3";
+    private static final String ELECTRICITY_UNIT = "kW/h";
+
     public static void main(String[] args) {
+        Counter hotWaterCounter = new Counter(HOT_WATER_COUNTER_NAME, WATER_UNIT);
+        Counter coldWaterCounter = new Counter(COLD_WATER_COUNTER_NAME, WATER_UNIT);
+        Counter electricityCounter = new Counter(ELECTRICITY_COUNTER_NAME, ELECTRICITY_UNIT, 240);
+
+        CounterService service = new CounterService(List.of(hotWaterCounter, coldWaterCounter));
+        service.addCounter(electricityCounter);
+        System.out.println(service.getCounterByName("Hot water"));
+        service.reset("Electricity");
+
+        System.out.println(service.getCounterByName(ELECTRICITY_COUNTER_NAME).getValue());
     }
 }
---
+++ src/com/walking/lesson37_collection_list/task1/service/CounterService.java
@@ -0,0 +1,73 @@
+package com.walking.lesson37_collection_list.task1.service;
+
+import com.walking.lesson37_collection_list.task1.model.Counter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Также реализовать сервис CounterService, зона ответственности которого —
+ * хранение массива доступных счетчиков, получение всех доступных
+ * счетчиков, получение доступа к счетчику по названию, увеличение значения
+ * счетчика на единицу или заданное значение, а также сброс счетчика до
+ * нулевого значения.
+ */
+public class CounterService {
+    private final List<Counter> counters;
+
+    public CounterService(List<Counter> counters) {
+        this.counters = new ArrayList<>(counters);
+
+    }
+
+    public Counter getCounterByName(String name) {
+        for (Counter counter : counters) {
+            if (counter.getName().equals(name)) {
+                return counter;
+            }
+        }
+
+        return null;
+    }
+
+    public List<Counter> getCounters() {
+        return counters;
+    }
+
+    public boolean addCounter(Counter newCounter) {
+        return counters.add(newCounter);
+    }
+
+    public void reset(String name) {
+        getCounterByName(name).setValue(0);
+        System.out.println("Counter is reset");
+    }
+
+    public void decrease(String name, int value) {
+        Counter counter = getCounterByName(name);
+        if (counter == null) {
+            throw new RuntimeException("This counter is empty");
+        }
+
+        counter.setValue(counter.getValue() - value);
+        System.out.println("Counter's value decreased");
+    }
+
+    public void decrement(String name) {
+        decrease(name, 1);
+    }
+
+    public void increase(String name, int value) {
+        Counter counter = getCounterByName(name);
+        if (counter == null) {
+            throw new RuntimeException("This counter is empty");
+        }
+
+        counter.setValue(counter.getValue() + value);
+        System.out.println("Counter's value increased");
+    }
+
+    public void increment(String name) {
+        increase(name, 1);
+    }
+}
