e188d2e95e19e603fa00db4deb7f9630cbbd13a0
AlexeyNiyazov <psychotrance92@yandex.ru>
2025-07-26 16:14:15 +1200

    Lesson 38 task2
    Signed-off-by: AlexeyNiyazov <psychotrance92@yandex.ru>

---
+++ src/com/walking/lesson38_comparing/task2/model/Car.java
@@ -0,0 +1,55 @@
+package com.walking.lesson38_comparing.task2.model;
+
+import org.jetbrains.annotations.NotNull;
+
+public class Car implements Comparable<Car> {
+    private final CarIdentifier identifier;
+    private String color;
+    private boolean actualTechnicalInspection;
+
+    public Car(String number, int year, String color, boolean actualTechnicalInspection) {
+        this(new CarIdentifier(number, year), color, actualTechnicalInspection);
+    }
+
+    public Car(CarIdentifier identifier, String color, boolean actualTechnicalInspection) {
+        this.identifier = identifier;
+        this.color = color;
+        this.actualTechnicalInspection = actualTechnicalInspection;
+    }
+
+    public CarIdentifier getIdentifier() {
+        return identifier;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isActualTechnicalInspection() {
+        return actualTechnicalInspection;
+    }
+
+    public void setActualTechnicalInspection(boolean actualTechnicalInspection) {
+        this.actualTechnicalInspection = actualTechnicalInspection;
+    }
+
+    @Override
+    public String toString() {
+        return """
+                number: %s
+                year: %d
+                color: %s
+                actualTechnicalInspection: %s
+                """.formatted(identifier.getNumber(), identifier.getYear(), color,
+                actualTechnicalInspection ? "actual" : "not actual");
+    }
+
+    @Override
+    public int compareTo(@NotNull Car o) {
+        return identifier.compareTo(o.getIdentifier());
+    }
+}
