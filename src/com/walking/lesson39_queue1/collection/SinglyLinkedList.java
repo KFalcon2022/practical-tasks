9dadf6c5bf9499574ce09bfa79dc91d67920bb7c
AlexeyNiyazov <psychotrance92@yandex.ru>
2025-07-29 16:27:05 +1200

    Lesson 39 task1
    Signed-off-by: AlexeyNiyazov <psychotrance92@yandex.ru>

---
+++ src/com/walking/lesson39_queue1/collection/SinglyLinkedList.java
@@ -0,0 +1,215 @@
+package com.walking.lesson39_queue1.collection;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+
+public class SinglyLinkedList<E> implements Collection<E> {
+    private Node<E> top;
+    private int size;
+
+    public SinglyLinkedList() {
+    }
+
+    public SinglyLinkedList(E element) {
+        this.top = new Node<>(element);
+        size++;
+    }
+
+    public E getTop() {
+        return top.value;
+    }
+
+    public void reverse() {
+        Node<E> prev = null;
+        while (top != null) {
+            Node<E> next = top.next;
+            top.next = prev;
+            prev = top;
+            top = next;
+        }
+
+        top = prev;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        Node<E> temp = top;
+
+        while (temp != null) {
+            if (Objects.equals(o, temp.value)) {
+                return true;
+            }
+
+            temp = temp.next;
+        }
+
+        return false;
+    }
+
+    @Override
+    public @NotNull Iterator<E> iterator() {
+        return new SinglyLinkedListIterator<>(top);
+    }
+
+    @Override
+    public Object @NotNull [] toArray() {
+        Object[] array = new Object[size];
+        Node<E> temp = top;
+
+        for (int i = 0; i < size; i++) {
+            array[i] = temp.value;
+            temp = temp.next;
+        }
+
+        return array;
+    }
+
+    @Override
+    public <T> T @NotNull [] toArray(T[] a) {
+        return (T[]) Arrays.copyOf(toArray(a), size, a.getClass());
+    }
+
+    @Override
+    public boolean add(E e) {
+        this.top = new Node<>(top, e);
+        size++;
+
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        if (Objects.equals(o, top.value)) {
+            top = top.next;
+            size--;
+
+            return true;
+        }
+
+        Node<E> temp = top;
+
+        while (temp != null) {
+            if (Objects.equals(o, temp.value)) {
+                temp = temp.next;
+                size--;
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        for (Object e : c) {
+            if (!contains(e)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean addAll(@NotNull Collection<? extends E> c) {
+        for (E e : c) {
+            add(e);
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean removeAll(@NotNull Collection<?> c) {
+        boolean removed = false;
+        Node<E> temp = top;
+
+        for (Object e : c) {
+            if (Objects.equals(temp.value, e)) {
+                temp = temp.next;
+                size--;
+
+                removed = true;
+            }
+        }
+
+        return removed;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        while (!c.contains(top.value)) {
+            top = top.next;
+            size--;
+        }
+
+        Node<E> temp = top;
+
+        while (temp.next != null) {
+            if (!c.contains(temp.next.value)) {
+                temp.next = temp.next.next;
+                size--;
+            }
+        }
+
+        return size > 0;
+    }
+
+    @Override
+    public void clear() {
+        top = null;
+        size = 0;
+    }
+
+    private static class Node<E> {
+        private Node<E> next;
+        private final E value;
+
+        public Node(E value) {
+            this.value = value;
+        }
+
+        public Node(Node<E> next, E value) {
+            this.next = next;
+            this.value = value;
+        }
+    }
+
+    private static class SinglyLinkedListIterator<E> implements Iterator<E> {
+        private Node<E> current;
+
+        public SinglyLinkedListIterator(Node<E> top) {
+            current = top;
+        }
+
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public E next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+
+            E result = current.value;
+            current = current.next;
+
+            return result;
+        }
+    }
+}
--- src/com/walking/lesson39_queue1/task1/Main.java
+++ src/com/walking/lesson39_queue1/task1/Main.java
@@ -1,5 +1,11 @@
 package com.walking.lesson39_queue1.task1;
 
+import com.walking.lesson39_queue1.collection.SinglyLinkedList;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.Iterator;
+
 /**
  * Реализуйте односвязный список (можно использовать реализацию в рамках
  * <a href="https://github.com/KFalcon2022/practical-tasks/blob/master/src/com/walking/lesson28_generics1/task4/structure/Stack.java">...</a>).
@@ -11,5 +17,36 @@ package com.walking.lesson39_queue1.task1;
  */
 public class Main {
     public static void main(String[] args) {
+        SinglyLinkedList<Integer> testIntegerList = createIntegerList();
+        testIntegerList.reverse();
+
+        for (Integer i : testIntegerList) {
+            System.out.println(i);
+        }
+    }
+
+    private static @NotNull SinglyLinkedList<Integer> createIntegerList() {
+        SinglyLinkedList<Integer> testIntegerList = new SinglyLinkedList<>();
+        testIntegerList.add(1);
+        testIntegerList.add(2);
+        testIntegerList.add(3);
+        testIntegerList.add(4);
+        testIntegerList.add(5);
+        testIntegerList.add(6);
+        testIntegerList.add(7);
+
+        return testIntegerList;
+    }
+
+    private static <E> void dropEvenHashCodeElement(Collection<E> c) {
+        Iterator<E> iterator = c.iterator();
+
+        while (iterator.hasNext()) {
+            E e = iterator.next();
+
+            if (e.hashCode() % 2 == 0) {
+                c.remove(e);
+            }
+        }
     }
 }
