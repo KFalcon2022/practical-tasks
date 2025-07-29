36fbb12d6e25acf592cce1568afacb5b5adfb36b
AlexeyNiyazov <psychotrance92@yandex.ru>
2025-07-29 16:49:45 +1200

    Lesson 39 task2
    Signed-off-by: AlexeyNiyazov <psychotrance92@yandex.ru>

--- src/com/walking/lesson39_queue1/task2/Main.java
+++ src/com/walking/lesson39_queue1/task2/Main.java
@@ -1,10 +1,31 @@
 package com.walking.lesson39_queue1.task2;
 
+import com.walking.lesson39_queue1.task2.collection.Stack;
+
 /**
  * Реализуйте структуру данных стек. Используйте односвязный список из Задачи 1.
  * При этом изменять сам класс, реализующий односвязный список, недопустимо.
  */
 public class Main {
     public static void main(String[] args) {
+        Stack<Integer> stack = createStack();
+
+        stack.push(7);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+    }
+
+    private static Stack<Integer> createStack() {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+
+        return stack;
     }
 }
