7c67c0c11e6888876dfbae1656bd29170ba6aa3b
AlexeyNiyazov <psychotrance92@yandex.ru>
2025-08-01 11:58:02 +1200

    Lesson 38 task3
    Signed-off-by: AlexeyNiyazov <psychotrance92@yandex.ru>

--- src/com/walking/lesson39_queue1/task3/Main.java
+++ src/com/walking/lesson39_queue1/task3/Main.java
@@ -1,5 +1,12 @@
 package com.walking.lesson39_queue1.task3;
 
+import com.walking.lesson39_queue1.collection.DoublyLinkedList;
+import com.walking.lesson39_queue1.collection.SinglyLinkedList;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.Iterator;
+
 /**
  * Реализуйте двусвязный список.
  * Реализуйте метод, разворачивающий список.
@@ -7,5 +14,44 @@ package com.walking.lesson39_queue1.task3;
  */
 public class Main {
     public static void main(String[] args) {
+        DoublyLinkedList<Integer> testIntegerList = new DoublyLinkedList<>();
+
+        testIntegerList.add(1);
+        testIntegerList.add(2);
+        testIntegerList.add(4);
+        testIntegerList.add(6);
+
+        testIntegerList.addTop(3);
+
+//        testIntegerList.reverse();
+
+        for (Integer i : testIntegerList) {
+            System.out.println(i);
+        }
+    }
+
+    private static @NotNull DoublyLinkedList<Integer> createIntegerList() {
+        DoublyLinkedList<Integer> testIntegerList = new DoublyLinkedList<>();
+        testIntegerList.add(1);
+        testIntegerList.add(2);
+        testIntegerList.add(3);
+        testIntegerList.add(4);
+        testIntegerList.add(5);
+        testIntegerList.add(6);
+        testIntegerList.add(7);
+
+        return testIntegerList;
+    }
+
+    private static <E> void dropEvenHashCodeElement(Collection<E> c) {
+        Iterator<E> iterator = c.iterator();
+
+        while (iterator.hasNext()) {
+            E e = iterator.next();
+
+            if (e.hashCode() % 2 == 0) {
+                c.remove(e);
+            }
+        }
     }
 }
