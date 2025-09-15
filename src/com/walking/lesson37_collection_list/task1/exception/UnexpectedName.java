ffd43b28b3ca997fb5aa79e7ccdb6201e97be871
Rita <margarita.mamatkazina@gmail.com>
2025-09-15 16:25:23 +0300

    lesson 37, task 1

--- src/com/walking/lesson37_collection_list/task1/exception/UnexpectedName.java
+++
@@ -1,7 +0,0 @@
-package com.walking.lesson37_collection_list.task1.exception;
-
-public class UnexpectedName extends RuntimeException {
-    public UnexpectedName(String message) {
-        super(message);
-    }
-}
