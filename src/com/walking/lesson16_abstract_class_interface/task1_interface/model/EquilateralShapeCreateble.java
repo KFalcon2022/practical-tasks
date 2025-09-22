2322893a087c1aaa87f02260b2e47259d5cedc25
thirteenth87 <ku.gordeev@gmail.com>
2025-09-22 15:36:04 +0500

    1 task 16 lesson

---
+++ src/com/walking/lesson16_abstract_class_interface/task1_interface/model/EquilateralShapeCreateble.java
@@ -0,0 +1,13 @@
+package com.walking.lesson16_abstract_class_interface.task1_interface.model;
+
+public interface EquilateralShapeCreateble {
+    String EMPTY_ELEMENT = " ";
+    String EMPTY_STRING = "";
+    char NEW_LINE_SYMBOL = '\n';
+
+    String createShapeString();
+
+    public int getLength();
+
+}
+
