8bc36d070c6dfd5b19e23c558a1282dc51327dc4
Rita <margarita.mamatkazina@gmail.com>
2025-12-09 15:04:23 +0300

    43, 1

---
+++ src/com/walking/lesson43_map/task1/exception/InvalidWordException.java
@@ -0,0 +1,7 @@
+package com.walking.lesson43_map.task1.exception;
+
+public class InvalidWordException extends RuntimeException {
+    public InvalidWordException(String message) {
+        super(message);
+    }
+}
