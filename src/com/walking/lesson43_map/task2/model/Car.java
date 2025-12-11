706f75478788c3b137581a3626fd2b861ce48acb
thirteenth87 <ku.gordeev@gmail.com>
2025-12-11 15:48:01 +0500

    lesson 43,46

---
+++ src/com/walking/lesson43_map/task2/model/Car.java
@@ -0,0 +1,102 @@
+package com.walking.lesson43_map.task2.model;
+
+import java.util.Objects;
+
+public class Car {
+    //    Уникальным идентификатором будем считать совокупность значений number и year
+
+    private final int year;
+
+    private String color;
+    private boolean actualTechnicalInspection;
+
+    //    Для ускорения поиска запишем хэшкод в отдельное поле. Это допустимо, учитывая,
+//    что хэшкод мы считаем лишь по неизменяемым полям и пересчитывать его
+//    при каждом вызове hashcode() бессмысленно.
+//    Похожий, но чуть более изощренный принцип использует класс String
+    private int hashcode;
+
+    public Car(int year, String color, boolean actualTechnicalInspection) {
+        this.year = year;
+        this.color = color;
+        this.actualTechnicalInspection = actualTechnicalInspection;
+
+//        Раз хэшкод де-факто неизменяем - почему бы не рассчитать его сразу?
+//        В реальном проекте это было бы избыточно, просто первый раз при поиск работал бы чуть медленнее
+//        (см. CarService).
+//        Но мы вызываем метод поиска лишь единожды, неприятно его осознанно замедлять
+        this.hashcode = hashCode();
+    }
+
+
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isActualTechnicalInspection() {
+        return actualTechnicalInspection;
+    }
+
+    public void setActualTechnicalInspection(boolean actualTechnicalInspection) {
+        this.actualTechnicalInspection = actualTechnicalInspection;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || !getClass().equals(o.getClass())) {
+            return false;
+        }
+
+        Car car = (Car) o;
+
+//        Учитываем в equals() лишь критичные для идентификации объекта поля.
+//        Класс Objects содержит множество методов для работы с объектами.
+//        equals(), hashcode(), проверки на null, сравнения и пр.
+//        Удобен в тех случаях, когда мы не уверены, что значение (или хотя бы одно из значений)
+//        не равно null
+        return year == car.year && Objects.equals(color, car.color);
+    }
+
+    @Override
+    public int hashCode() {
+//        Если значение hashcode == 0 (значение поля типа int по умолчанию) - хэшкод еще не рассчитывался
+//        (или поля заполнены 0 и null, но в рамках задачи для нас это не критично)
+        if (hashcode == 0) {
+//            рассчитаем хэшкод
+            hashcode = color.hashCode();
+            hashcode += 31 * year;
+        }
+
+//        если же хэшкод != 0 - его уже считали, смысла пересчитывать нет, т.к. исходные поля неизменяемы.
+//        Просто вернем предрассчитанное значение
+        return hashcode;
+    }
+
+    @Override
+    public String toString() {
+//        Метод formatted() у String позволяет создать строку с использованием спецификаторов
+//        (как в System.out.printf()). До Java 15 вместо него использовался статический метод String.format().
+//        Здесь он использован для большей наглядности кода, менее красиво, но с тем же успехом,
+//        можно было обойтись обычной конкатенацией строк
+        return """
+                year: %d
+                color: %s
+                actualTechnicalInspection: %s
+                """.formatted(
+                year, color,
+                actualTechnicalInspection ? "actual" : "not actual");
+    }
+}
