e5320856876cd3ca66847e590dd0b6fcee935b86
Rita <margarita.mamatkazina@gmail.com>
2025-12-23 15:49:42 +0300

    46, 1

--- src/com/walking/lesson46_lambda/task1/Main.java
+++ src/com/walking/lesson46_lambda/task1/Main.java
@@ -1,5 +1,11 @@
 package com.walking.lesson46_lambda.task1;
 
+import com.walking.lesson46_lambda.task1.model.Car;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * Реализуйте
  * <a href="https://github.com/KFalcon2022/practical-tasks/tree/master/src/com/walking/lesson38_comparing/task1">Задачу 1 из урока 38</a>
@@ -7,5 +13,54 @@ package com.walking.lesson46_lambda.task1;
  */
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Car> cars = new ArrayList<>(createList());
+        sortByNumber(cars);
+        printList(cars);
+    }
+
+    public static void sortByOwner(List<Car> cars) {
+        cars.sort(Comparator.comparing(o -> o.getOwner()));
+    }
+
+    public static void sortByNumber(List<Car> cars) {
+        cars.sort(Comparator.comparing(o -> o.getIdentifier().getNumber()));
+    }
+
+    public static void sortByBrand(List<Car> cars) {
+        cars.sort(Comparator.comparing(o -> o.getIdentifier().getBrand()));
+    }
+
+    public static void sortByIdentifier(List<Car> cars) {
+        Comparator<Car> comparator = (o1, o2) -> {
+            int result = o1.getIdentifier()
+                    .getBrand()
+                    .compareTo(o2.getIdentifier().getBrand());
+            if (result == 0) {
+                return o1.getIdentifier()
+                        .getNumber()
+                        .compareTo(o2.getIdentifier().getNumber());
+            }
+            return result;
+        };
+        cars.sort(comparator);
+    }
+
+    public static List<Car> createList() {
+        return List.of(
+                new Car("Audi", "Печкин Ф.Б.", "уе777к"),
+                new Car("BMW", "Борисов Ю.А.", "ос025р"),
+                new Car("Ford", "Снегирь Ю.В.", "мр666м"),
+                new Car("Bentley", "Белова Д.М.", "ра345з"),
+                new Car("Ferrari", "Сергеев К.Л.", "уе999з"),
+                new Car("Москвич", "Попов А.Д.", "ар832с"),
+                new Car("Audi", "Васильев П.Д.", "кв865а"),
+                new Car("BMW", "Лебедев А.Ю.", "ау000ш"),
+                new Car("Москвич", "Алексеев Л.Б.", "гн123г"));
+    }
+
+    public static void printList(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car);
+        }
     }
 }
