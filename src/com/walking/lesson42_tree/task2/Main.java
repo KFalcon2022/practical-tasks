79138d7f1d9b7396ae0fa8da715f2e0dd96557d7
AlexeyNiyazov <psychotrance92@yandex.ru>
2026-01-06 01:31:41 +1200

    Lesson 42 task2 AVL_Tree: not ended(plz check) AVL_tree_2: from youtube with correction
    Signed-off-by: AlexeyNiyazov <psychotrance92@yandex.ru>

--- src/com/walking/lesson42_tree/task2/Main.java
+++ src/com/walking/lesson42_tree/task2/Main.java
@@ -1,6 +1,7 @@
 package com.walking.lesson42_tree.task2;
 
 import com.walking.lesson42_tree.task2.structure.AVL_Tree;
+import com.walking.lesson42_tree.task2.structure.AVL_tree_2;
 
 /**
  * Реализуйте АВЛ-дерево.
@@ -9,23 +10,29 @@ import com.walking.lesson42_tree.task2.structure.AVL_Tree;
  */
 public class Main {
     public static void main(String[] args) {
-        AVL_Tree<Integer> tree = new AVL_Tree<>();
-        tree.add(30);
-        tree.add(57);
-        tree.add(18);
-        tree.add(65);
-        tree.add(40);
-        tree.add(9);
-        tree.add(96);
-        tree.add(24);
-        tree.add(12);
-        tree.add(98);
-        tree.add(62);
-        tree.add(55);
-        tree.add(31);
-        tree.add(20);
+//        AVL_Tree<Integer> tree = new AVL_Tree<>();
+        AVL_tree_2<Integer> tree = new AVL_tree_2<>();
+        for (int i = 9; i > 0; i--) {
+            tree.add(i);
+        }
+
+//        tree.add(30);
+//        tree.add(57);
+//        tree.add(18);
+//        tree.add(65);
+//        tree.add(40);
+//        tree.add(9);
+//        tree.add(96);
+//        tree.add(24);
+//        tree.add(12);
+//        tree.add(98);
+//        tree.add(62);
+//        tree.add(55);
+//        tree.add(31);
+//        tree.add(20);
 
 
-        System.out.println("lvl: " + tree.getLvl());
+        tree.delete(6);
+        System.out.println("lvl: " + tree.getTreeHeight());
     }
 }
