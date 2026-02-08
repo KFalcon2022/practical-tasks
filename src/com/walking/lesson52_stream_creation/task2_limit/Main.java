0e3ec4831e11f1fcc3ec74af4d5ff5bfbd654dda
AlexeyNiyazov <psychotrance92@yandex.ru>
2026-02-08 20:58:42 +1200

    Lesson 52 task 1-2 (3rd won't be)
    Signed-off-by: AlexeyNiyazov <psychotrance92@yandex.ru>

--- src/com/walking/lesson52_stream_creation/task2_limit/Main.java
+++ src/com/walking/lesson52_stream_creation/task2_limit/Main.java
@@ -1,10 +1,18 @@
 package com.walking.lesson52_stream_creation.task2_limit;
 
+import java.time.LocalDate;
+import java.util.stream.Stream;
+
 /**
  * Реализуйте программу, выводящую в консоль все даты текущего месяца.
  * Используйте Stream.limit()
  */
 public class Main {
     public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+
+        Stream.iterate(1, i -> ++i)
+                .limit(now.lengthOfMonth())
+                .forEach(i -> System.out.printf("%d.%d.%d\n", i, now.getMonthValue(), now.getYear()));
     }
 }
