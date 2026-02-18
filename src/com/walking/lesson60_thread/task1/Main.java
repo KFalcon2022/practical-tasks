533095d519c09ff8ac7df0865696293a449f638b
AlexeyNiyazov <psychotrance92@yandex.ru>
2026-02-18 15:40:24 +1200

    Lesson 60 task1
    Signed-off-by: AlexeyNiyazov <psychotrance92@yandex.ru>

--- src/com/walking/lesson60_thread/task1/Main.java
+++ src/com/walking/lesson60_thread/task1/Main.java
@@ -1,10 +1,29 @@
 package com.walking.lesson60_thread.task1;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Напишите программу, которая пишет в консоль текущее время каждый две секунды,
  * пока программа запущена.
  */
 public class Main {
     public static void main(String[] args) {
+        getTimeEvery2Seconds();
+    }
+
+    private static void getTimeEvery2Seconds() {
+        while (true) {
+            var t = new Thread(() ->
+                    System.out.println(LocalTime.now()
+                            .format(DateTimeFormatter.
+                                    ofPattern("hh.mm.ss"))));
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            t.start();
+        }
     }
 }
