c9e5eb05ba5f1b5236c9402d8e6124404cbeacc7
Kasyanov Maxim <maxim.kasyanov@gmail.com>
2026-07-26 10:18:41 +0300

    Вариант решения задания №1 из 14 урока. https://github.com/KFalcon2022/lessons/blob/master/lessons/java-core/014/Polymorphism.%20Overriding%20method.%20Types%20of%20polymorphism.md

--- src/com/walking/lesson14_polymorphism/task1/Main.java
+++ src/com/walking/lesson14_polymorphism/task1/Main.java
@@ -10,7 +10,40 @@ package com.walking.lesson14_polymorphism.task1;
  * <p>
  * Обратите внимание, символ '\' в Java необходимо экранировать: '\\'.
  */
+import com.walking.lesson14_polymorphism.task1.model.Square;
+import com.walking.lesson14_polymorphism.task1.model.Triangle;
+
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    static void main() {
+        new Main().run();
+    }
+
+    private void run(){
+        try(Scanner input = new Scanner(System.in)) {
+
+            System.out.print("Нажмите 1 для рисования квадрата или 2 для рисования треугольника: ");
+            byte shapeVariant = input.nextByte();
+
+            switch(shapeVariant)
+            {
+                case 1:
+                    // Квадрат
+                    System.out.print("Укажите длину стороны квадрата: ");
+                    Square square = new Square(input.nextInt());
+                    square.drawShape();
+                    break;
+                case 2:
+                    // Треугольник
+                    System.out.print("Укажите длину боковой стороны треугольника: ");
+                    Triangle triangle = new Triangle(input.nextInt());
+                    triangle.drawShape();
+                    break;
+                default:
+                    System.out.println("Неизвестный тип фигуры");
+                    break;
+            }
+        }
     }
 }
